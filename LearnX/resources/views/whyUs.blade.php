d13480b3aa7302a7d6e44471c3e4f67827373474
kirby-b <kirby.brodie66@gmail.com>
2023-11-20 18:23:43 -0500

    Update whyUs.blade.php
    Probably will need to make it wider

--- LearnX/resources/views/whyUs.blade.php
+++ LearnX/resources/views/whyUs.blade.php
@@ -2,28 +2,37 @@
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
             <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
-                <!-- Image Section -->
-                <div class="md:w-1/2 p-4">
-                    <img src="/Images/Teacher-Online-With-Student.jpg" alt="Teacher Online With Student"
-                        class="max-w-full h-auto rounded-lg shadow-lg">
-                </div>
 
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
-                    <p class="text-xl font-sans m-4">
-                        LearnX, a place for students K-12 to strive and grow through their educational learning.
-                        We strive to make every year count as your students grow through this program. At LearnX,
-                        we take it upon ourselves to better our platform for students and for you. We teach kids
-                        their 123's, all the way up to their f(x) = 3x^4 - 2x^3 + 5x^2 - 7x + 9 (The answer is
-                        x≈0.5231),
-                        from their ABC's to full essays on complex topics our world faces. For this upcoming 2024-2025
-                        school year, we hope to help teachers engage with their students more than they could. We
-                        also offer personalized learning sessions so students can get the most out of their learning.
-                        We make our system user-friendly so you can get the most out of our site.
-                        <br><br>
-                        We hope you enjoy LearnX!
-                        <a href="{{ route('signin') }}" class="text-blue-600 hover:text-blue-800">Click here</a>
-                        to make a new account.
+                    <p class="text-xl font-sans m-4">                        
+                        <h1 class="text-center underline">Why Us?</h1>
+                        <div class="font-sans text-2xl h-auto w-356.25">
+                            <p>
+                            We offer revelent courses to suit the students needs. Our flashcards and small assignments will fit<br>  
+                            what the student would learn during the course of that year. We run our assignments through our <br>
+                            employees and other teachers throughout the U.S. to ensure that every student gets the correct <br>
+                            information they will need to learn as best as they can. With LearnX students get the best education <br>
+                            they can get. Our website is easy to navigate and easy for teachers to help students. Teachers can <br>
+                            add their own assignments and eaisily put them into the gradebook. All of our assignments, quizzes, <br>
+                            and tests challenge and push students as they grow. We don't have any premium account because <br>
+                            of our belifs that every child deserves a fair and free education. We do ask for donations which you<br> 
+                            can find at the bottom of the screen.
+                            </p>
+                        </div>
+                        <div class="border-solid border-5 border-amber-700">
+                            <div>
+                                <img src="/Images/Calculator-On-Notepad.jpg" width="400px" height="450px" class="float-right" ><!--DON'T PUT ANYTHING HERE, THIS IS FOR PIC ONLY-->
+                            </div>
+                            <div class="text-2xl font-sans">
+                                LearnX made learning in school so much easier! -Larua Don<br>
+                                <br>
+                                School and LearnX equals success. - Kelly Hemp<br>
+                            </div>
+                            <div class="text-2xl font-sans">
+                                All schools should use LearnX! My child is so successful in school now! -Brooke Asher
+                            </div>
+                        </div>
                     </p>
                 </div>
             </div>
