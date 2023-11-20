dba2384d4080e2dd73b83e1acc7d345744af8346
kirby-b <kirby.brodie66@gmail.com>
2023-11-20 18:17:17 -0500

    FAQ
    Should look good

--- LearnX/resources/views/FAQ.blade.php
+++ LearnX/resources/views/FAQ.blade.php
@@ -10,21 +10,49 @@
 
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
-                    <p class="text-xl font-sans m-4">
-                        LearnX, a place for students K-12 to strive and grow through their educational learning.
-                        We strive to make every year count as your students grow through this program. At LearnX,
-                        we take it upon ourselves to better our platform for students and for you. We teach kids
-                        their 123's, all the way up to their f(x) = 3x^4 - 2x^3 + 5x^2 - 7x + 9 (The answer is
-                        x≈0.5231),
-                        from their ABC's to full essays on complex topics our world faces. For this upcoming 2024-2025
-                        school year, we hope to help teachers engage with their students more than they could. We
-                        also offer personalized learning sessions so students can get the most out of their learning.
-                        We make our system user-friendly so you can get the most out of our site.
-                        <br><br>
-                        We hope you enjoy LearnX!
-                        <a href="{{ route('signin') }}" class="text-blue-600 hover:text-blue-800">Click here</a>
-                        to make a new account.
-                    </p>
+                    <div class="text-xl font-sans m-4">
+                        <div class="text-2xl font-serif">
+                            <h1 class="text-center">FAQ</h1>
+                            <h4>What is better about LearnX?</h4>
+                            <p>LearnX is a simple-to-use online website for K-12 to learn and grow. We offer class assignments and 
+                            tailor to the students so we can best help their learning needs. We make learning fun by using a belt
+                            system and as students go up grade levels, their belt colors will upgrade.</p>
+                            <br>
+                            <h4>How secure will my child's and the school's information be kept?</h4>
+                            <p>We encrypt all information we hold so students information can be kept safe and they can learn with no worry.</p>
+                            <br>
+                            <h4>What is the belt system?</h4>
+                            <p>The belt system gives a fun way for K-12 students to learn by color coding each grade. The color will become
+                            more prominent as they get father into the grade.</p>
+                            <br>
+                            <h4>What will my kid be learning?</h4>
+                            <p>Any assignments on our site are the bare minimum for that grade level. Teachers are still responsible for most assignments. We
+                            help teach concepts but do not provide assignments for anything extra the teacher might want to add.</p>
+                            <br>
+                            <h4>How can I contact LearnX if there is a problem?</h4>
+                            <p>LearnX can be contacted at learnxinfo@learnx.org for any problems regarding our site, assignments, or other technical problem
+                            there may be with our site.</p>
+                            <br>
+                            <!--<h4>What are the terms of service?</h4>
+                            <p>Dont be bad</p>
+                            <br>-->
+                            <h4>Is this site mobile-friendly?</h4>
+                            <p>We are currently working on our site becoming more mobile-friendly. We hope you enjoy the desktop version while you wait!</p>
+                            <br>
+                            <h4>Does this site cost money?</h4>
+                            <p>We do not have any "premium" accounts. We ask for donations only which you can find <a type="button" id="inText" value ="Yes" href={{ route("page.donate") }}>here</a></p>
+                            <br>
+                            <h4>Can I trust the information on this site?</h4>
+                            <p>Yes. We do research and run all problems through our employees and other teachers in the U.S. so you can always be assured 
+                            that your students will never be given anything other than accuracy.</p>
+                            <br>
+                            <h4>Can I customize student's experience?</h4>
+                            <p>You can add assignments for your students to do but we do not offer a complete remodel of the system.</p>
+                            <br>
+                            <!--<h4>Is there a helper forum?</h4>
+                            <p>There are resources to help you work through our site.</p>-->
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
