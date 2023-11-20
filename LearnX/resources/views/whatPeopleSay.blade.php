6bbeaac80a1510f2973111310e24921949573425
kirby-b <kirby.brodie66@gmail.com>
2023-11-20 18:21:22 -0500

    Update whatPeopleSay.blade.php
    Added from comment to main

--- LearnX/resources/views/whatPeopleSay.blade.php
+++ LearnX/resources/views/whatPeopleSay.blade.php
@@ -2,29 +2,24 @@
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
             <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
-                <!-- Image Section -->
-                <div class="md:w-1/2 p-4">
-                    <img src="/Images/Teacher-Online-With-Student.jpg" alt="Teacher Online With Student"
-                        class="max-w-full h-auto rounded-lg shadow-lg">
-                </div>
 
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
-                    <p class="text-xl font-sans m-4">
-                        LearnX, a place for students K-12 to strive and grow through their educational learning.
-                        We strive to make every year count as your students grow through this program. At LearnX,
-                        we take it upon ourselves to better our platform for students and for you. We teach kids
-                        their 123's, all the way up to their f(x) = 3x^4 - 2x^3 + 5x^2 - 7x + 9 (The answer is
-                        x≈0.5231),
-                        from their ABC's to full essays on complex topics our world faces. For this upcoming 2024-2025
-                        school year, we hope to help teachers engage with their students more than they could. We
-                        also offer personalized learning sessions so students can get the most out of their learning.
-                        We make our system user-friendly so you can get the most out of our site.
-                        <br><br>
-                        We hope you enjoy LearnX!
-                        <a href="{{ route('signin') }}" class="text-blue-600 hover:text-blue-800">Click here</a>
-                        to make a new account.
-                    </p>
+                    <div class="text-xl font-sans m-4">
+                        <h1 class="text-center">What Students and Teachers Say About Learn</h1>
+                        <p class="text-left text-xl indent-0.5">
+                            Olivia Smith(K-Student): "I like the colorful pictures and fun stories on LearnX. It makes learning so much fun!"<br>
+                            Sarah Anderson (Teacher): "LearnX provides a variety of teaching resources and lesson plans that make my job as a teacher easier. It helps me keep my students engaged."<br>
+                            Ethan Davis(3rd Grade Student): "LearnX has interactive quizzes that help me understand the lessons better. It's not boring like some other stuff."<br>
+                            James Mitchell (Teacher): "I appreciate the flexibility of LearnX in adapting to different learning styles. It allows me to tailor lessons to meet the unique needs of my students."<br>
+                            Sophia Roberts (High School Student): "LearnX has a lot of great study materials and practice tests that help me prepare for my exams. It's a huge help for high school."<br>
+                            Emily Parker (Teacher): "LearnX offers a wide range of educational tools and assessments that help me track my students' progress and adjust my teaching methods accordingly."<br>
+                            Ryan Lewis (High School Student): "LearnX is my go-to resource for test preparation and self-study. It offers a wide range of study materials and practice tests that are helping me excel in high school."<br>
+                            Michael Turner (Teacher): "The professional development resources on LearnX are invaluable. They keep me updated with the latest teaching strategies and techniques."<br>
+                            Ava Wilson (High School Student): "LearnX is user-friendly and helps me manage my assignments and deadlines. It makes my high school life less stressful."<br>
+                            Lisa Johnson (Teacher): "LearnX has a fantastic platform for creating and sharing educational content. It's a great tool for collaborating with other educators and sharing resources, making my teaching job more efficient."<br>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
