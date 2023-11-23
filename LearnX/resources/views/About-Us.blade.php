d7469561601adf56306390f4ed5eac6f959cd031
kirby-b <kirby.brodie66@gmail.com>
2023-11-22 21:32:50 -0500

    About-Us
    -Images align with their own text
    -Looks good small screened
    -Doesn't look dumb

--- LearnX/resources/views/About-Us.blade.php
+++ LearnX/resources/views/About-Us.blade.php
@@ -1,30 +1,115 @@
 <x-partials.layout>
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
-            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
-                <!-- Image Section -->
-                <div class="md:w-1/2 p-4">
-                    <img src="/Images/Adelaide_Meisel.png" alt="Our Founder, Adelaide"
-                        class="max-w-full h-auto rounded-lg shadow-lg">
-                </div>
+            <div class="flex flex-col justify-center items-center md:space-x-6">
+                <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+                    <!-- Image Section -->
+                    <div class="md:w-1/2 p-4">
+                        <img src="/Images/Adelaide_Meisel.png" alt="Our Founder, Adelaide"
+                            class="max-w-full h-auto rounded-lg shadow-lg">
+                    </div>
 
-                <!-- Text Section -->
-                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
-                    <p class="text-xl font-serif m-4">
-                        I am the founder of LearnX. I have designed this website to be easy to interact with, easy to teach, 
-                        and easy to learn. I want students to be able to learn the fundamentals of education through our site. Thank you 
-                        for supporting us. 
-                        <br>
-                        <br>
-                        Equality: At LearnX we treat all of our students with equality. We make sure that all students have access to the 
-                        same information, our donations go to help them, and all students learn the same information regardless of race, 
-                        sex, wealth etc.
-                        <br>
-                        Teamwork: We organize ourselves to collaborate and manage LearnX efficiently with teamwork. We consider LearnX to
-                        be a team building project and work to make the site and ourselves better. We are always providing feedback to 
-                        our employees and helping them thrive in our organization. We understand that working with each other and you 
-                        helps students to grow which is why teamwork is so important to us. 
-                    </p>
+                    <!-- Text Section -->
+                    <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                        <p class="text-xl font-serif m-4">
+                            I am the founder of LearnX. I have designed this website to be easy to interact with, easy to teach, 
+                            and easy to learn. I want students to be able to learn the fundamentals of education through our site. Thank you 
+                            for supporting us. 
+                            <br>
+                            <br>
+                            Equality: At LearnX we treat all of our students with equality. We make sure that all students have access to the 
+                            same information, our donations go to help them, and all students learn the same information regardless of race, 
+                            sex, wealth etc.
+                            <br>
+                            Teamwork: We organize ourselves to collaborate and manage LearnX efficiently with teamwork. We consider LearnX to
+                            be a team building project and work to make the site and ourselves better. We are always providing feedback to 
+                            our employees and helping them thrive in our organization. We understand that working with each other and you 
+                            helps students to grow which is why teamwork is so important to us. 
+                        </p>
+                    </div>
+                </div>
+                <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+                    <!-- Image Section -->
+                    <div class="md:w-1/2 p-4">
+                        <img src="/Images/Brodie_Kirby.png" alt="Our Founder, Adelaide"
+                            class="max-w-full h-auto rounded-lg shadow-lg">
+                    </div>
+
+                    <!-- Text Section -->
+                    <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                        <p class="text-xl font-serif m-4">
+                            I am the co-founder of LearnX. I helped build this website from the ground up with the goal of 
+                            bringing good education to all. I have always wanted to lead something important and helpful for the world, 
+                            and here at LearnX I finally have the chance. I am a programming hobbyist and hope to be one of the leading 
+                            supports for any teacher who wishes to bring programming to the classroom. By working at LearnX I understand 
+                            the importance of working hard and working towards your goals as a student.
+                            <br>
+                            <br>
+                            Honesty: Here at LearnX we hold honesty very closely. As an online school platform, we understand how people can be 
+                            skeptical of us and how well students may work compared to traditional school settings. But we assure you we will 
+                            be transparent and help to make sure your child works just as hard on our platform as in a normal school. 
+                            <br>
+                            Responsiblity: At LearnX we care a lot about responsibility. The most successful people must be very responsible to 
+                            get as far as they do, and just as much so to stay there. We make sure any student who uses our platform has the 
+                            ability to learn how to be responsible and work their way to the top.
+                        </p>
+                    </div>
+                </div>
+                <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+                    <!-- Image Section -->
+                    <div class="md:w-1/2 p-4">
+                        <img src="/Images/Cameron_Hamilton.png" alt="Our Founder, Adelaide"
+                            class="max-w-full h-auto rounded-lg shadow-lg">
+                    </div>
+
+                    <!-- Text Section -->
+                    <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                        <p class="text-xl font-serif m-4">
+                            I'm fortunate to be part of the team behind LearnX, to transform the way people learn. Collaborating with a 
+                            group of dedicated individuals, I've been able to contribute to the development of this innovative learning 
+                            platform. I've gained valuable experience as we strive to make education more accessible and engaging. It's 
+                            been a rewarding journey, knowing that my work plays a part in helping learners of all backgrounds expand 
+                            their knowledge. I look forward to the future as we continue to refine and improve LearnX.
+                            <br>
+                            <br>
+                            Respect: LearnX is a platform that fosters a culture of respect by promoting inclusive and diverse learning 
+                            environments, where individuals from various backgrounds and perspectives are encouraged to engage in 
+                            meaningful discussions.
+                            <br>
+                            Open-mindedness: Open-mindedness is a core value at LearnX, encouraging learners to explore new ideas and viewpoints with 
+                            curiosity, while also valuing the contributions of others, leading to a more enriching educational experience. 
+                        </p>
+                    </div>
+                </div>
+                <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+                    <!-- Image Section -->
+                    <div class="md:w-1/2 p-4">
+                        <img src="/Images/Thomas_Whitaker.png" alt="Our Founder, Adelaide"
+                            class="max-w-full h-auto rounded-lg shadow-lg">
+                    </div>
+
+                    <!-- Text Section -->
+                    <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                        <p class="text-xl font-serif m-4">
+                            I work at LearnX to revolutionize education for K-12 students, creating an innovative online platform that 
+                            caters to diverse learning styles. Through research and collaboration with educators, LearnX has created a 
+                            comprehensive curriculum for core subjects, interactive lessons, and engaging activities to make learning 
+                            enjoyable. With a commitment to continuous improvement, "LearnX" strives to empower students with the skills 
+                            and knowledge necessary for success in the ever-changing world.
+                            <br>
+                            <br>
+                            <br>
+                            Courage: The spark that ignites the flame of curiosity in young minds is courage. It serves as the gateway to 
+                            exploration, empowering children to venture into the realms of knowledge with confidence. By fostering courage,
+                            we teach resilience, ensuring that every challenge becomes an opportunity for growth and discovery.
+                            <br>
+                            <br>
+                            Freedom: The key that unlocks the boundless potential of young learners is freedom. It provides children with 
+                            the liberty to navigate through a diverse array of educational experiences. Through exploration, LearnX inspires 
+                            a sense of ownership in learning, encouraging children to chart their own course toward a future enriched with 
+                            knowledge and creativity.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
