493b940bdf109510447cf385392a149af08e278b
kirby-b <kirby.brodie66@gmail.com>
2023-11-22 21:41:02 -0500

    Deleted un-needed comments from files
    -Effected files:
    --About-us
    --Donate
    --FAQ
    --whatPeopleSay
    --whyUs

--- LearnX/resources/views/whatPeopleSay.blade.php
+++ LearnX/resources/views/whatPeopleSay.blade.php
@@ -25,21 +25,3 @@
         </div>
     </div>
 </x-partials.layout>
-{{-- <div class="bg-purple-500">
-
-        <h1 class="text-center">What Students and Teachers Say About Learn</h1>
-
-        <p class="text-left text-xl indent-0.5">
-            Olivia Smith(K-Student): "I like the colorful pictures and fun stories on LearnX. It makes learning so much fun!"<br><br>
-            Sarah Anderson (Teacher): "LearnX provides a variety of teaching resources and lesson plans that make my job as a teacher easier. It helps me keep my students engaged."<br><br>
-            Ethan Davis(3rd Grade Student): "LearnX has interactive quizzes that help me understand the lessons better. It's not boring like some other stuff."<br><br>
-            James Mitchell (Teacher): "I appreciate the flexibility of LearnX in adapting to different learning styles. It allows me to tailor lessons to meet the unique needs of my students."<br><br>
-            Sophia Roberts (High School Student): "LearnX has a lot of great study materials and practice tests that help me prepare for my exams. It's a huge help for high school."<br><br>
-            Emily Parker (Teacher): "LearnX offers a wide range of educational tools and assessments that help me track my students' progress and adjust my teaching methods accordingly."<br><br>
-            Ryan Lewis (High School Student): "LearnX is my go-to resource for test preparation and self-study. It offers a wide range of study materials and practice tests that are helping me excel in high school."<br><br>
-            Michael Turner (Teacher): "The professional development resources on LearnX are invaluable. They keep me updated with the latest teaching strategies and techniques."<br><br>
-            Ava Wilson (High School Student): "LearnX is user-friendly and helps me manage my assignments and deadlines. It makes my high school life less stressful."<br><br>
-            Lisa Johnson (Teacher): "LearnX has a fantastic platform for creating and sharing educational content. It's a great tool for collaborating with other educators and sharing resources, making my teaching job more efficient."<br><br>
-        </p>
-
-    </div>  --}}
--- LearnX/resources/views/whyUs.blade.php
+++ LearnX/resources/views/whyUs.blade.php
@@ -37,35 +37,3 @@
         </div>
     </div>
 </x-partials.layout>
-{{-- <div class="bg-purple-400">
-    
-        <p class="text-center underline">Why Us?</p>
-        <div class="font-sans text-2xl h-auto w-356.25">
-            <p>
-            We offer revelent courses to suit the students needs. Our flashcards and small assignments will fit<br>  
-            what the student would learn during the course of that year. We run our assignments through our <br>
-            employees and other teachers throughout the U.S. to ensure that every student gets the correct <br>
-            information they will need to learn as best as they can. With LearnX students get the best education <br>
-            they can get. Our website is easy to navigate and easy for teachers to help students. Teachers can <br>
-            add their own assignments and eaisily put them into the gradebook. All of our assignments, quizzes, <br>
-            and tests challenge and push students as they grow. We don't have any premium account because <br>
-            of our belifs that every child deserves a fair and free education. We do ask for donations which you<br> 
-            can find at the bottom of the screen.
-            </p>
-        </div>
-        <div class="border-solid border-5 border-amber-700">
-            <div>
-                <img src="/Images/Calculator-On-Notepad.jpg" width="400px" height="450px" class="float-right" ><!--DON'T PUT ANYTHING HERE, THIS IS FOR PIC ONLY-->
-            </div>
-            <div class="text-2xl font-sans">
-                LearnX made learning in school so much easier! -Larua Don<br>
-                <br>
-                School and LearnX equals success. - Kelly Hemp<br>
-            </div>
-            <div class="text-2xl font-sans">
-                All schools should use LearnX! My child is so successful in school now! -Brooke Asher
-            </div>
-        </div>
-
-
-    </div> --}}
