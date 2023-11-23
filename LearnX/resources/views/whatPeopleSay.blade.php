c27d078625240ed6bd65d5b52c85edb874b2d1c7
kirby-b <kirby.brodie66@gmail.com>
2023-11-22 21:37:04 -0500

    Update whatPeopleSay.blade.php
    -Fixed text size
    -Italicized names for quotes

--- LearnX/resources/views/whatPeopleSay.blade.php
+++ LearnX/resources/views/whatPeopleSay.blade.php
@@ -6,18 +6,18 @@
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <div class="text-xl font-sans m-4">
-                        <h1 class="text-center">What Students and Teachers Say About Learn</h1>
-                        <p class="text-left text-xl indent-0.5">
-                            Olivia Smith(K-Student): "I like the colorful pictures and fun stories on LearnX. It makes learning so much fun!"<br>
-                            Sarah Anderson (Teacher): "LearnX provides a variety of teaching resources and lesson plans that make my job as a teacher easier. It helps me keep my students engaged."<br>
-                            Ethan Davis(3rd Grade Student): "LearnX has interactive quizzes that help me understand the lessons better. It's not boring like some other stuff."<br>
-                            James Mitchell (Teacher): "I appreciate the flexibility of LearnX in adapting to different learning styles. It allows me to tailor lessons to meet the unique needs of my students."<br>
-                            Sophia Roberts (High School Student): "LearnX has a lot of great study materials and practice tests that help me prepare for my exams. It's a huge help for high school."<br>
-                            Emily Parker (Teacher): "LearnX offers a wide range of educational tools and assessments that help me track my students' progress and adjust my teaching methods accordingly."<br>
-                            Ryan Lewis (High School Student): "LearnX is my go-to resource for test preparation and self-study. It offers a wide range of study materials and practice tests that are helping me excel in high school."<br>
-                            Michael Turner (Teacher): "The professional development resources on LearnX are invaluable. They keep me updated with the latest teaching strategies and techniques."<br>
-                            Ava Wilson (High School Student): "LearnX is user-friendly and helps me manage my assignments and deadlines. It makes my high school life less stressful."<br>
-                            Lisa Johnson (Teacher): "LearnX has a fantastic platform for creating and sharing educational content. It's a great tool for collaborating with other educators and sharing resources, making my teaching job more efficient."<br>
+                        <h1 class="text-center text-2xl">What Students and Teachers Say About Learn</h1>
+                        <p class="text-xl">
+                            <i>Olivia Smith(K-Student):</i> "I like the colorful pictures and fun stories on LearnX. It makes learning so much fun!"<br>
+                            <i>Sarah Anderson (Teacher):</i> "LearnX provides a variety of teaching resources and lesson plans that make my job as a teacher easier. It helps me keep my students engaged."<br>
+                            <i>Ethan Davis(3rd Grade Student):</i> "LearnX has interactive quizzes that help me understand the lessons better. It's not boring like some other stuff."<br>
+                            <i>James Mitchell (Teacher):</i> "I appreciate the flexibility of LearnX in adapting to different learning styles. It allows me to tailor lessons to meet the unique needs of my students."<br>
+                            <i>Sophia Roberts (High School Student):</i> "LearnX has a lot of great study materials and practice tests that help me prepare for my exams. It's a huge help for high school."<br>
+                            <i>Emily Parker (Teacher):</i> "LearnX offers a wide range of educational tools and assessments that help me track my students' progress and adjust my teaching methods accordingly."<br>
+                            <i>Ryan Lewis (High School Student):</i> "LearnX is my go-to resource for test preparation and self-study. It offers a wide range of study materials and practice tests that are helping me excel in high school."<br>
+                            <i>Michael Turner (Teacher):</i> "The professional development resources on LearnX are invaluable. They keep me updated with the latest teaching strategies and techniques."<br>
+                            <i>Ava Wilson (High School Student):</i> "LearnX is user-friendly and helps me manage my assignments and deadlines. It makes my high school life less stressful."<br>
+                            <i>Lisa Johnson (Teacher):</i> "LearnX has a fantastic platform for creating and sharing educational content. It's a great tool for collaborating with other educators and sharing resources, making my teaching job more efficient."<br>
                         </p>
                     </div>
                 </div>
