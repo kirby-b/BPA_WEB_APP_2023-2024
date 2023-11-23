ff8623b2a22650af1465e3b504f8a2b826e66685
kirby-b <kirby.brodie66@gmail.com>
2023-11-22 21:33:48 -0500

    Donate
    -Donation looks good
    -Updated button and text
    -still does nothing

--- LearnX/resources/views/donate.blade.php
+++ LearnX/resources/views/donate.blade.php
@@ -6,10 +6,11 @@
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg flex justify-center items-center">
                     <div class="text-xl font-sans m-4">
-                        <h1 class="text-center">LearnX Can't Do It Alone</h1>
-                        <p class="text-center text-lg">
+                        <h1 class="text-center text-4xl">LearnX Can't Do It Alone</h1>
+                        <p class="text-center text-xl">
                             We apperciate your donation, please type in the amount you wish to donate below:
                         </p>
+                        <br>
                         <form method="POST" action="/users/authenticate" class="">
                             @csrf <!--Makes it so people cant use scripts against your site-->
                             <div class="justify-center items-center">
@@ -18,12 +19,10 @@
                                     <input class="w-75 h-15 border-black border-solid border-2 p-3 text-2xl" type="text" id="dollar" name="dollar" placeholder="Dollar Amt" value="{{old('dollar')}}">
                                 </div>
                                     <br>
-                                    <br>
                                     Credit/Debit Card #
                                 <div>
                                     <input class="w-75 h-15 border-black border-solid border-2 p-3 text-2xl" type="text" id="Cred-Deb" name="Cred-Deb" placeholder="Card ####-####-####-####" value="{{old('Cred-Deb')}}">
                                 </div>
-                                    <br>
                                     <br>
                                     CVC
                                 <div>
@@ -31,7 +30,7 @@
                                 </div>
                                     <!--submit button-->
                                 <div class="">
-                                    <input class="font-Preahvihear text-3xl no-underline border-none cursor-pointer bg-black" type="submit" id="submit" name="submit" value="DONATE>">
+                                    <input class="font-Preahvihear text-3xl no-underline border-none cursor-pointer text-black" type="submit" id="submit" name="submit" value="DONATE>">
                                 </div>
                                 <!--Gets your input and tries to verify your account info. endError sections determine what will happen if they input the
                                     wrong info -->
