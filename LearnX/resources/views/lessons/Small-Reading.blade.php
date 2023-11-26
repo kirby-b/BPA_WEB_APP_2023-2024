a27f0ad2bcb2ac3ab4c4915391c9c7f2578a7a08
kirby-b <kirby.brodie66@gmail.com>
2023-11-26 14:02:43 -0500

    Create Small-Reading.blade.php
    -added initial content and code

---
+++ LearnX/resources/views/lessons/Small-Reading.blade.php
@@ -0,0 +1,19 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div style="font-family: 'Preahvihear';Text-align: Center; font-size:5rem;">
+                            <b>SMALL READING</b>
+                        </div>
+
+                        <!--and, here, my, you, dog, etc-->
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
