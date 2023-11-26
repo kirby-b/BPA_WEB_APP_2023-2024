6db2110c4efc55e475742beb0a1670c0696a6f51
kirby-b <kirby.brodie66@gmail.com>
2023-11-26 14:01:48 -0500

    Create Contractions.blade.php
    -added initial content

---
+++ LearnX/resources/views/lessons/Contractions.blade.php
@@ -0,0 +1,40 @@
+<x-partials.UserLayout>
+    <div style="font-family: 'Preahvihear';Text-align: Center; font-size:5rem;">
+        <b>CONTRACTIONS</b>
+    </div>
+    <div style="display: flex; flex-direction: colum;"> 
+        <div style="font-family: 'Arial'; width: 50%; margin-left: 24%">
+            I am → I'm<br>
+            Example: I'm going to the store.<br>
+            <br>
+            you are → you're<br>
+            Example: Do you know where you're going?<br>
+            <br>
+            they are → they're<br>
+            Example: They're coming to the party.<br>
+            <br>
+            we are → we're<br>
+            Example: We're excited about the trip.<br>
+            <br>
+            it is → it's<br>
+            Example: It's a beautiful day.<br>
+            <br>
+        </div>
+        <div style="font-family: 'Arial'; width: 50%; text-align:left">
+            can not → can't<br>
+            Example: I can't believe it.<br>
+            <br>
+            will not → won't<br>
+            Example: She won't be late.<br>
+            <br>
+            does not → doesn't<br>
+            Example: He doesn't like coffee.<br>
+            <br>
+            is not → isn't<br>
+            Example: This isn't what I expected.<br>
+            <br>
+            are not → aren't<br>
+            Example: They aren't coming to the meeting.<br>
+        </div>
+    </div>
+</x-partials.UserLayout>
