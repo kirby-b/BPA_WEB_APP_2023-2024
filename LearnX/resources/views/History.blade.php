1f376f69972a86cfc35f718c0935aa78c83b56d7
kirby-b <kirby.brodie66@gmail.com>
2023-11-26 12:40:02 -0500

    Update History.blade.php
    -Updated History page with everything in the right place
    -Deleted unnecessary comments

--- LearnX/resources/views/History.blade.php
+++ LearnX/resources/views/History.blade.php
@@ -3,59 +3,50 @@
         <div class="container mx-auto px-4">
             <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
 
+                <!-- Activity Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <div class="font-Preahvihear text-3xl">
+                        <div class="text-center bg-purple-400">Flashcards</div>
+                        <div class="text-center bg-green-400">Learning Fun</div>
+                        <div class="text-center bg-purple-400">Timeline</div>
+                    </div>
+                </div>
+
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
-                        LearnX, a place for students K-12 to strive and grow through their educational learning.
-                        We strive to make every year count as your students grow through this program. At LearnX,
-                        we take it upon ourselves to better our platform for students and for you. We teach kids
-                        their 123's, all the way up to their f(x) = 3x^4 - 2x^3 + 5x^2 - 7x + 9 (The answer is
-                        x≈0.5231),
-                        from their ABC's to full essays on complex topics our world faces. For this upcoming 2024-2025
-                        school year, we hope to help teachers engage with their students more than they could. We
-                        also offer personalized learning sessions so students can get the most out of their learning.
-                        We make our system user-friendly so you can get the most out of our site.
-                        <br><br>
-                        We hope you enjoy LearnX!
-                        <a href="{{ route('signin') }}" class="text-blue-600 hover:text-blue-800">Click here</a>
-                        to make a new account.
+                        <div class="font-Preahvihear text-center text-4xl">
+                            <b>History</b>
+                        </div>
+                        <div style="flex flex-col"> 
+                            <div class="font-sans w-50/100 float-right">
+                                <div>Holidays Around the World</div>
+                                <br>
+                                <div>Contenents/Oceans</div>
+                                <br>
+                                <div>States in the U.S.</div>
+                                <br>
+                                <div>Branches of the U.S. Government</div>
+                                <br>
+                                <div>Ancient History</div>
+                                <br>
+                            </div> 
+                            <div class="font-sans w-50/100 float-left">
+                                <div>American History</div>
+                                <br>
+                                <div>World History</div>
+                                <br>
+                                <div>Economics</div>
+                                <br>
+                                <div>Civics</div>
+                                <br>
+                                <div>Politics</div>
+                                <br>
+                            </div>
+                        </div>
                     </p>
                 </div>
             </div>
         </div>
     </div>
 </x-partials.UserLayout>
-{{-- <div class="font-Preahvihear text-center text-7xl">
-    <b>HISTORY</b>
-</div>
-    <div style="flex flex-col"> 
-        <div class="font-sans w-50/100 float-right">
-            <div>Holidays Around the World</div>
-            <br>
-            <div>Contenents/Oceans</div>
-            <br>
-            <div>States in the U.S.</div>
-            <br>
-            <div>Branches of the U.S. Government</div>
-            <br>
-            <div>Ancient History</div>
-            <br>
-        </div> 
-        <div class="font-sans w-50/100 float-left">
-            <div>American History</div>
-            <br>
-            <div>World History</div>
-            <br>
-            <div>Economics</div>
-            <br>
-            <div>Civics</div>
-            <br>
-            <div>Politics</div>
-            <br>
-        </div>
-    </div>
-<div class="font-Preahvihear text-3xl text-left h-24 w-full">
-    <div class="bg-purple-400">Flashcards</div>
-    <div class="bg-green-400">Learning Fun</div>
-    <div class="bg-purple-400">Timeline</div>
-</div> --}}
