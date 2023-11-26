3afb7083d7e79a204f9e2804cd7e44ce939e9f73
kirby-b <kirby.brodie66@gmail.com>
2023-11-26 14:02:26 -0500

    Create Parts-Of-Speech.blade.php
    -added initial content and layout

---
+++ LearnX/resources/views/lessons/Parts-Of-Speech.blade.php
@@ -0,0 +1,41 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div style="font-family: 'Preahvihear';Text-align: Center; font-size:5rem;">
+                            <b>PARTS OF SPEECH</b>
+                        </div>
+                        <div style="margin-left: 20%">
+                            Nouns: Words that represent people, places, things, or ideas. <br>
+                            Examples: dog, city, love<br>
+                            <br>
+                            Verbs: Words that describe actions or states of being.<br>
+                            Examples: run, eat, think<br>
+                            <br>
+                            Adjectives: Words that modify or describe nouns, giving more information about them.<br>
+                            Examples: happy, tall, blue<br>
+                            <br>
+                            Adverbs: Words that modify or describe verbs, adjectives, or other adverbs, providing more details about how, when, where, or to what extent.<br>
+                            Examples: quickly, very, often<br>
+                            <br>
+                            Pronouns: Words that take the place of nouns.<br>
+                            Examples: he, she, it, they<br>
+                            <br>
+                            Conjunctions: Words that connect words, phrases, or clauses.<br>
+                            Examples: and, but, or<br>
+                            <br>
+                            Articles: The words "a," "an," and "the," which are used to specify or limit a noun<br>
+                            Examples: I saw a cat in the backyard./Can you please pass the salt?
+                            <br>
+                        </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
+        
