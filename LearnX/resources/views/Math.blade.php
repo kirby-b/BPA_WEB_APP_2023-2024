5adc473ff5e4a9f9596b8bbae32e56ffee9db3ae
kirby-b <kirby.brodie66@gmail.com>
2023-11-26 12:40:23 -0500

    Update Math.blade.php
    -Updated math page with everything in the right place
    -Deleted unnecessary comments

--- LearnX/resources/views/Math.blade.php
+++ LearnX/resources/views/Math.blade.php
@@ -2,61 +2,51 @@
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
             <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
-                
+                <!-- Activity Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <div class="font-Preahvihear text-3xl">
+                        <div class="text-center bg-purple-400">Flashcards</div>
+                        <div class="text-center bg-green-400">Learning Fun</div>
+                        <div class="text-center bg-purple-400">Solve It</div>
+                    </div>
+                </div>
+
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
-                        LearnX, a place for students K-12 to strive and grow through their educational learning.
-                        We strive to make every year count as your students grow through this program. At LearnX,
-                        we take it upon ourselves to better our platform for students and for you. We teach kids
-                        their 123's, all the way up to their f(x) = 3x^4 - 2x^3 + 5x^2 - 7x + 9 (The answer is
-                        x≈0.5231),
-                        from their ABC's to full essays on complex topics our world faces. For this upcoming 2024-2025
-                        school year, we hope to help teachers engage with their students more than they could. We
-                        also offer personalized learning sessions so students can get the most out of their learning.
-                        We make our system user-friendly so you can get the most out of our site.
-                        <br><br>
-                        We hope you enjoy LearnX!
-                        <a href="{{ route('signin') }}" class="text-blue-600 hover:text-blue-800">Click here</a>
-                        to make a new account.
+                        <div class="font-Preahvihear text-center text-4xl">
+                            <b>MATH</b>
+                        </div>
+                        <div style="flex flex-col"> 
+                            <div class="font-sans w-50/100 float-right">
+                                <br>
+                                <div>Numbers</div>
+                                <br>
+                                <div>Shapes & Colors</div>
+                                <br>
+                                <div>Probability</div>
+                                <br>
+                                <div>Elementary Math</div>
+                                <br>
+                                <div>Graphing</div>
+                            </div> 
+                            <div class="font-sans w-50/100 float-left">
+                                <br>
+                                <div>Staticstics</div>
+                                <br> 
+                                <div>Fractions, Decimals, and Precentages</div>
+                                <br>
+                                <div>Alagrbra(I and II)</div><!--make link to a page where they choose-->
+                                <br>
+                                <div>Geometry</div>
+                                <br>
+                                <div>Calculus</div>
+                                <br>
+                            </div>
+                        </div>
                     </p>
                 </div>
             </div>
         </div>
     </div>
 </x-partials.UserLayout>
-{{-- <div class="font-Preahvihear text-center text-7xl">
-        <b>MATH</b>
-    </div>
-        <div style="flex flex-col"> 
-            <div class="font-sans w-50/100 float-right">
-                <br>
-                <div>Numbers</div>
-                <br>
-                <div>Shapes & Colors</div>
-                <br>
-                <div>Probability</div>
-                <br>
-                <div>Elementary Math</div>
-                <br>
-                <div>Graphing</div>
-            </div> 
-            <div class="font-sans w-50/100 float-left">
-                <br>
-                <div>Staticstics</div>
-                <br> 
-                <div>Fractions, Decimals, and Precentages</div>
-                <br>
-                <div>Alagrbra(I and II)</div><!--make link to a page where they choose-->
-                <br>
-                <div>Geometry</div>
-                <br>
-                <div>Calculus</div>
-                <br>
-            </div>
-        </div>
-    <div class="font-Preahvihear text-3xl text-left h-24 w-full">
-        <div class="bg-purple-400">Flashcards</div>
-        <div class="bg-green-400">Learning Fun</div>
-        <div class="bg-purple-400">Solve It</div>
-    </div> --}}
