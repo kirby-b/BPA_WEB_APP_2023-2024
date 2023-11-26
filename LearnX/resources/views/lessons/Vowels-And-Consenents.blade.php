ebd6142934443580335a2264b5d1097ff98ccaa9
kirby-b <kirby.brodie66@gmail.com>
2023-11-26 14:03:04 -0500

    Create Vowels-And-Consenents.blade.php
    -added initial content and layout

---
+++ LearnX/resources/views/lessons/Vowels-And-Consenents.blade.php
@@ -0,0 +1,24 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div style="font-family: 'Preahvihear';Text-align: Center; font-size:5rem;">
+                        <b>VOWELS & CONSONANTS</b>
+                        </div>
+                        <div style="font-family: Arial, Helvetica, sans-serif; text-align:center;">
+                        A,E,I,O, and U are vowels. Vowels are letters that appear in every word and every syllable.
+                        <br>
+                        <br>
+                        All the other lettersare consonants and work with the vowels to create words
+                        </div>
+                        {{-- <div style="margin-left:50px; margin-top: 300px;"><img src="/Images/Vowels.png" alt="Image cannot load" width="287px" height="109px"></div><!--Image by Freepik--> --}}
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
