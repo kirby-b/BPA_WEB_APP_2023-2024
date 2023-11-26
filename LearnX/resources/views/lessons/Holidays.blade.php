b3262f87c562024571c10ff3dcbe7b75dbc76540
kirby-b <kirby.brodie66@gmail.com>
2023-11-26 14:02:07 -0500

    Create Holidays.blade.php
    -added initial content and code

---
+++ LearnX/resources/views/lessons/Holidays.blade.php
@@ -0,0 +1,42 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div style="font-family: 'Preahvihear';Text-align: Center; font-size:5rem;">
+                            <b>HOLIDAYS</b>
+                        </div>
+                        <div style="display: flex; flex-direction: colum;"> 
+                            <div style="font-family: 'Arial'; width: 50%; margin-left: 24%">
+                                China<br>
+                                <br>
+                                India<br>
+                                <br>
+                                United States<br>
+                                <br>
+                                Brazil<br>
+                                <br>
+                                Russia<br>
+                            </div>
+                            <div style="font-family: 'Arial'; width: 50%; text-align:left">
+                                Indonsia<br>
+                                <br>
+                                Pakistan<br>
+                                <br>
+                                Nigeria<br>
+                                <br>
+                                Japan<br>
+                                <br>
+                                Mexico<br>
+                            </div>
+                        </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
+        
