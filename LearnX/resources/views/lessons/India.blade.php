d24607e543999b032304c7981c4f52c5d6d12984
Kirby-b <kirby.brodie66@gmail.com>
2023-11-27 10:21:10 -0500

    Create India.blade.php

---
+++ LearnX/resources/views/lessons/India.blade.php
@@ -0,0 +1,44 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>HOLIDAYS</b>
+                        </div>
+                        <div class="font-Preahvihear items-center text-xl">
+                            <b>India</b>
+                        </div>
+                        <div class="flex flex-col"> 
+                            <div class="font-sans">
+                                Diwali:<br>Festival of Lights, celebrated in October or November.<br>
+                                <br>
+                                Holi:<br>Festival of Colors, celebrated in March.<br>
+                                <br>
+                                Independence Day:<br>Celebrated on August 15.<br>
+                                <br>
+                                Raksha Bandhan:<br>Celebrated in August, a festival celebrating the bond between brothers and sisters.<br>
+                                <br>
+                            <div class="font-sans">
+                                Ganesh Chaturthi: Celebrated in August or September in honor of Lord Ganesha.<br>
+                                <br>
+                                Durga Puja:<br>A major Hindu festival celebrated in October.<br>
+                                <br>
+                                Navaratri/Dussehra:<br>Celebrated in September or October, honoring the goddess Durga.<br>
+                                <br>
+                                Eid al-Fitr:<br>Celebrated at the end of Ramadan.<br>
+                                <br>
+                                Eid al-Adha:<br>Celebrated by Muslims, known as the "Festival of Sacrifice."<br>
+                                <br>
+                                Republic Day:<br>Celebrated on January 26.<br>
+                            </div>
+                        </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
