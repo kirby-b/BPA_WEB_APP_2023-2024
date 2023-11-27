4342926767b33911e4cde947a282451f9160d82e
Kirby-b <kirby.brodie66@gmail.com>
2023-11-27 10:21:23 -0500

    Update Reasearch-Citations.blade.php

--- LearnX/resources/views/lessons/Reasearch-Citations.blade.php
+++ LearnX/resources/views/lessons/Reasearch-Citations.blade.php
@@ -6,7 +6,7 @@
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
-                        <div style="font-family: 'Preahvihear';Text-align: Center; font-size:5rem;">
+                        <div class="font-Preahvihear">
                             <b>REASEARCH/CITATIONS</b>
                         </div>
 
