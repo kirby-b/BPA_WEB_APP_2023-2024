5adaa37f25a98ed397db85c65c5c2d212d8c0a37
Kirby-b <kirby.brodie66@gmail.com>
2023-11-27 10:22:13 -0500

    Update Math.blade.php

--- LearnX/resources/views/Math.blade.php
+++ LearnX/resources/views/Math.blade.php
@@ -17,7 +17,7 @@
                         <div class="font-Preahvihear text-center text-4xl">
                             <b>MATH</b>
                         </div>
-                        <div style="flex flex-col"> 
+                        <div ="flex flex-col"> 
                             <div class="font-sans w-50/100 float-right">
                                 <br>
                                 <div>Numbers</div>
