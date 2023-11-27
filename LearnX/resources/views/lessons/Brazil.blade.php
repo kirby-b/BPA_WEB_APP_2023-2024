efb2ab24751abc0f69713b69469a245e3f200da9
Kirby-b <kirby.brodie66@gmail.com>
2023-11-27 10:20:50 -0500

    Create Brazil.blade.php

---
+++ LearnX/resources/views/lessons/Brazil.blade.php
@@ -0,0 +1,44 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>HOLIDAYS</b>
+                        </div>
+                        <div class="font-Preahvihear items-center text-xl">
+                            <b>Brazil</b>
+                        </div>
+                        <div class="flex flex-col"> 
+                            <div class="font-sans">
+                                Carnival(varies, usually February):<br>One of the world's largest and most famous festivals.<br>
+                                <br>
+                                Independence Day: Celebrated on September 7.<br>
+                                <br>
+                                Tiradentes Day: Commemorates a national hero, celebrated on April 21.<br>
+                                <br>
+                                Festa Junina: Celebrated in June with traditional Brazilian folk festivals.<br>
+                                <br>
+                                Independence of Bahia: Celebrated on July 2, commemorating the independence of the state of Bahia.<br>
+                                <br>
+                            <div class="font-sans">
+                                Corpus Christi: A Christian holiday celebrated in June.<br>
+                                <br>
+                                Brazilian Valentine's Day(Dia dos Namorados): Celebrated on June 12.<br>
+                                <br>
+                                Proclamation of the Republic: Celebrated on November 15.<br>
+                                <br>
+                                Immaculate Conception Day: Celebrated on December 8.<br>
+                                <br>
+                                Labour Day(Dia do Trabalhador): Celebrated on May 1.<br>
+                            </div>
+                        </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
