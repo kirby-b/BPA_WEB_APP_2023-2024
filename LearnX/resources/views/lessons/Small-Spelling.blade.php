81b6ed35cdbd389d03e0ef736982c11d0070522c
Kirby-b <kirby.brodie66@gmail.com>
2023-11-27 10:21:30 -0500

    Update Small-Spelling.blade.php

--- LearnX/resources/views/lessons/Small-Spelling.blade.php
+++ LearnX/resources/views/lessons/Small-Spelling.blade.php
@@ -6,7 +6,7 @@
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
-                        <div style="font-family: 'Preahvihear';Text-align: Center; font-size:5rem;">
+                        <div class="font-Preahvihear items-center text-2xl">
                             <b>SMALL SPELLING</b>
                         </div>
 
