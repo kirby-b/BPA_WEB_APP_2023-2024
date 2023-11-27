bd8bdb253574d7149e4c3c0c259c3695a73d524c
Kirby-b <kirby.brodie66@gmail.com>
2023-11-27 10:21:43 -0500

    Create United-States.blade.php

---
+++ LearnX/resources/views/lessons/United-States.blade.php
@@ -0,0 +1,44 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>HOLIDAYS</b>
+                        </div>
+                        <div class="font-Preahvihear items-center text-xl">
+                            <b>United States</b>
+                        </div>
+                        <div class="flex flex-col"> 
+                            <div class="font-sans ">
+                                Independence Day(July 4):<br>Celebrated with fireworks and patriotic events.<br>
+                                <br>
+                                Thanksgiving Day:<br>Celebrated on the fourth Thursday in November.<br>
+                                <br>
+                                Christmas:<br>Celebrated on December 25.<br>
+                                <br>
+                                Memorial Day:<br>Honors the military personnel who have died, observed on the last Monday of May.<br>
+                                <br>
+                                Labor Day:<br>Celebrated on the first Monday in September, honoring the contributions of American workers.<br>
+                                <br>
+                            <div class="font-sans">
+                                Martin Luther King Jr. Day:<br>Commemorates the civil rights leader, observed on the third Monday in January.<br>
+                                <br>
+                                Easter:<br>Celebrated in April, commemorating the resurrection of Jesus Christ.<br>
+                                <br>
+                                Veterans Day:<br>Honors military veterans, observed on November 11.<br>
+                                <br>
+                                New Year's Day(January 1):<br>Celebrated at the beginning of the year.<br>
+                                <br>
+                                Columbus Day:<br>Celebrated on the second Monday in October.
+                            </div>
+                        </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
