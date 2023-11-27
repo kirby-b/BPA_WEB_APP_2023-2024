9d644362ad9ec663c4cb3f68082944f2133cad69
Kirby-b <kirby.brodie66@gmail.com>
2023-11-27 10:22:19 -0500

    Create Russia.blade.php

---
+++ LearnX/resources/views/lessons/Russia.blade.php
@@ -0,0 +1,44 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>HOLIDAYS</b>
+                        </div>
+                        <div class="font-Preahvihear items-center text-xl">
+                            <b>Russia</b>
+                        </div>
+                        <div class="flex flex-col"> 
+                            <div class="font-sans">
+                                New Year's Day(January 1): Celebrated with festive events and Ded Moroz.<br>
+                                <br>
+                                Victory Day(May 9):<br>Commemorates the victory over Nazi Germany.<br>
+                                <br>
+                                Russian Orthodox Christmas:<br>Celebrated on January 7.<br>
+                                <br>
+                                Defender of the Fatherland Day:<br>Celebrated on February 23, honoring the Russian Armed Forces.<br>
+                                <br>
+                                International Women's Day:<br>Celebrated on March 8, honoring women's contributions.<br>
+                                <br>
+                            <div class="font-sans">
+                                Unity Day:<br>Celebrated on November 4, commemorating the Russian unity against foreign invaders.<br>
+                                <br>
+                                Cosmonautics Day:<br>Celebrated on April 12, honoring space exploration.<br>
+                                <br>
+                                May Day(Spring and Labor Day):<br>Celebrated on May 1.<br>
+                                <br>
+                                Constitution Day:<br>Celebrated on December 12.<br>
+                                <br>
+                                Day of National Unity:<br>Celebrated on November 4.<br>
+                            </div>
+                        </div>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
