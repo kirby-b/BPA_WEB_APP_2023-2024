48d92ae732a64e3321291e4fe80748d5fcde2724
Kirby-b <kirby.brodie66@gmail.com>
2023-11-28 10:04:06 -0500

    Update welcome.blade.php
    -Added FAQ, WhyUs, and Reviews to main
    - Made id's for sectioning
    - New layout name
    -Other small changes

--- LearnX/resources/views/welcome.blade.php
+++ LearnX/resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
-<x-partials.layout>
+<x-partials.Mainlayout>
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
             <!--Main Section-->
-            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6" id="main">
                 <!-- Image Section -->
                 <div class="md:w-1/2 p-4">
                     <img src="/Images/Teacher-Online-With-Student.jpg" alt="Teacher Online With Student"
@@ -27,36 +27,40 @@
                     </p>
                 </div>
             </div>
+            <br>
+            <br>
             <!--Reviews Section-->
-            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6" id="reviews">
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <div class="text-xl font-sans m-4">
-                        <h1 class="text-center text-2xl">What Students and Teachers Say About Learn</h1>
+                        <h1 class="text-center text-2xl">What Students and Teachers Say About LearnX</h1>
                         <p class="text-xl">
-                            <i>Olivia Smith(K-Student):</i> "I like the colorful pictures and fun stories on LearnX. It makes learning so much fun!"<br>
-                            <i>Sarah Anderson (Teacher):</i> "LearnX provides a variety of teaching resources and lesson plans that make my job as a teacher easier. It helps me keep my students engaged."<br>
-                            <i>Ethan Davis(3rd Grade Student):</i> "LearnX has interactive quizzes that help me understand the lessons better. It's not boring like some other stuff."<br>
-                            <i>James Mitchell (Teacher):</i> "I appreciate the flexibility of LearnX in adapting to different learning styles. It allows me to tailor lessons to meet the unique needs of my students."<br>
-                            <i>Sophia Roberts (High School Student):</i> "LearnX has a lot of great study materials and practice tests that help me prepare for my exams. It's a huge help for high school."<br>
-                            <i>Emily Parker (Teacher):</i> "LearnX offers a wide range of educational tools and assessments that help me track my students' progress and adjust my teaching methods accordingly."<br>
-                            <i>Ryan Lewis (High School Student):</i> "LearnX is my go-to resource for test preparation and self-study. It offers a wide range of study materials and practice tests that are helping me excel in high school."<br>
-                            <i>Michael Turner (Teacher):</i> "The professional development resources on LearnX are invaluable. They keep me updated with the latest teaching strategies and techniques."<br>
-                            <i>Ava Wilson (High School Student):</i> "LearnX is user-friendly and helps me manage my assignments and deadlines. It makes my high school life less stressful."<br>
-                            <i>Lisa Johnson (Teacher):</i> "LearnX has a fantastic platform for creating and sharing educational content. It's a great tool for collaborating with other educators and sharing resources, making my teaching job more efficient."<br>
+                            <i>Olivia Smith(K-Student):</i> "I like the colorful pictures and fun stories on LearnX. It makes learning so much fun!"<br><br>
+                            <i>Sarah Anderson (Teacher):</i> "LearnX provides a variety of teaching resources and lesson plans that make my job as a teacher easier. It helps me keep my students engaged."<br><br>
+                            <i>Ethan Davis(3rd Grade Student):</i> "LearnX has interactive quizzes that help me understand the lessons better. It's not boring like some other stuff."<br><br>
+                            <i>James Mitchell (Teacher):</i> "I appreciate the flexibility of LearnX in adapting to different learning styles. It allows me to tailor lessons to meet the unique needs of my students."<br><br>
+                            <i>Sophia Roberts (High School Student):</i> "LearnX has a lot of great study materials and practice tests that help me prepare for my exams. It's a huge help for high school."<br><br>
+                            <i>Emily Parker (Teacher):</i> "LearnX offers a wide range of educational tools and assessments that help me track my students' progress and adjust my teaching methods accordingly."<br><br>
+                            <i>Ryan Lewis (High School Student):</i> "LearnX is my go-to resource for test preparation and self-study. It offers a wide range of study materials and practice tests that are helping me excel in high school."<br><br>
+                            <i>Michael Turner (Teacher):</i> "The professional development resources on LearnX are invaluable. They keep me updated with the latest teaching strategies and techniques."<br><br>
+                            <i>Ava Wilson (High School Student):</i> "LearnX is user-friendly and helps me manage my assignments and deadlines. It makes my high school life less stressful."<br><br>
+                            <i>Lisa Johnson (Teacher):</i> "LearnX has a fantastic platform for creating and sharing educational content. It's a great tool for collaborating with other educators and sharing resources, making my teaching job more efficient."<br><br>
                         </p>
                     </div>
                 </div>
             </div>
+            <br>
+            <br>
             <!--Why Us Section-->
-            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6" id="whyus">
                 <!-- Quotes Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">                        
-                        <p class="text-center underline text-3xl">Quotes?</p>
+                        <p class="text-center underline text-3xl">Quotes</p>
                         <div class="border-solid border-5 border-amber-700">
                             <div class="text-2xl font-sans">
-                                LearnX made learning in school so much easier! -Larua Don<br><br>
+                                LearnX made learning in school so much easier for my child! -Larua Don<br><br>
                                 School and LearnX equals success. - Kelly Hemp<br><br>
                                 All schools should use LearnX! My child is so successful in school now! -Brooke Asher
                             </div>
@@ -81,8 +85,10 @@
                     </p>
                 </div>
             </div>
+            <br>
+            <br>
             <!--FAQ Section-->
-            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6" id="faq">
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <div class="text-xl font-sans m-4">
@@ -132,4 +138,4 @@
             </div>
         </div>
     </div>
-</x-partials.layout>
+</x-partials.Mainlayout>
