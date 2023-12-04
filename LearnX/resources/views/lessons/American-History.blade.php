f3b70be5310e091e475bd365fce2ec0df47e3fdc
Kirby-b <kirby.brodie66@gmail.com>
2023-12-04 10:19:35 -0500

    Update American-History.blade.php

--- LearnX/resources/views/lessons/American-History.blade.php
+++ LearnX/resources/views/lessons/American-History.blade.php
@@ -6,7 +6,19 @@
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
-                        
+                        15th Century<br>
+                        <br>
+                        16th VEnturey<br>
+                        <br>
+                        17th Century<br>
+                        <br>
+                        18th Century<br>
+                        <br>
+                        19th Century<br>
+                        <br>
+                        20th Century<br>
+                        <br>
+                        21st Century<br>
                     </p>
                 </div>
             </div>
