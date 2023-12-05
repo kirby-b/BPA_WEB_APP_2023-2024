45548d266ec2aa20f5809a89c288016ab212e85e
Kirby-b <kirby.brodie66@gmail.com>
2023-12-05 10:14:06 -0500

    Create 21st-Century.blade.php
    Added facts as well

---
+++ LearnX/resources/views/lessons/History/American/21st-Century.blade.php
@@ -0,0 +1,26 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                    21st Century:<br>
+                    <br>
+                    2001: The 9/11 terrorist attacks.<br>
+                    2008: The global financial crisis.<br>
+                    2010: The Affordable Care Act (Obamacare) is signed into law.<br>
+                    2015: The Supreme Court legalizes same-sex marriage nationwide.<br>
+                    2020: The COVID-19 pandemic significantly impacts the U.S. and the world.<br>
+                    2020: Black Lives Matter protests gain momentum following the death of George Floyd.<br>
+                    2020: Presidential election with Joe Biden elected as the 46th President.<br>
+                    2021: Inauguration of Joe Biden and Kamala Harris.<br>
+                    2021: Capitol riot on January 6th.<br>
+                    2021: COVID-19 vaccine distribution begins.<br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
