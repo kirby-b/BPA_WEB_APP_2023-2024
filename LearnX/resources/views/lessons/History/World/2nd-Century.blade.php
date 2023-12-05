4fedc6fb8f08635f59580d4feca12b0ef8988236
Kirby-b <kirby.brodie66@gmail.com>
2023-12-05 10:18:08 -0500

    Create 2nd-Century.blade.php
    added new files

---
+++ LearnX/resources/views/lessons/History/World/2nd-Century.blade.php
@@ -0,0 +1,28 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>2nd Century</b>
+                        </div>
+                        <br>
+                        105: Han Dynasty collapses, leading to the Three Kingdoms period.
+                        105-106: Roman Emperor Trajan's conquest of Dacia.
+                        105-114: Roman Emperor Trajan's campaigns in Dacia.
+                        132-136: Bar Kokhba revolt against Roman rule in Judea.
+                        143-163: Antonine Plague.
+                        166-180: Reign of Roman Emperor Marcus Aurelius.                        
+                        161: The Roman Empire reaches its height under Antoninus Pius.
+                        180: End of the Pax Romana.
+                        184: Yellow Turban Rebellion in China.
+                        226: Fall of the Parthian Empire and rise of the Sassanian Empire.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
