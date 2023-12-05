cdbd9fe3fcfdb35983d9e877d303611fcd8063f2
Kirby-b <kirby.brodie66@gmail.com>
2023-12-05 10:13:56 -0500

    Create 19th-Century.blade.php
    Added facts as well

---
+++ LearnX/resources/views/lessons/History/American/19th-Century.blade.php
@@ -0,0 +1,26 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                    19th Century:<br>
+                    <br>
+                    1803: The Louisiana Purchase.<br>
+                    1812-1815: The War of 1812.<br>
+                    1820: The Missouri Compromise is enacted.<br>
+                    1830: The Indian Removal Act.<br>
+                    1836: The Battle of the Alamo during the Texas Revolution.<br>
+                    1846-1848: The Mexican-American War.<br>
+                    1850: The Compromise of 1850.<br>
+                    1862: The Homestead Act is signed into law.<br>
+                    1863: The Emancipation Proclamation is issued.<br>
+                    1869: The First Transcontinental Railroad is completed.<br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
