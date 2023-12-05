2b66c194a13ee3380ccb37188fddd18e9e72133b
Kirby-b <kirby.brodie66@gmail.com>
2023-12-05 10:18:33 -0500

    Create World-History.blade.php
    items to make into links and also made a new folder

---
+++ LearnX/resources/views/lessons/History/World/World-History.blade.php
@@ -0,0 +1,55 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        1st<br>
+                        <br>
+                        2nd<br>
+                        <br>
+                        3rd<br>
+                        <br>
+                        4th<br>
+                        <br>
+                        5th<br>
+                        <br>
+                        6th<br>
+                        <br>
+                        7th<br>
+                        <br>
+                        8th<br>
+                        <br>
+                        9th<br>
+                        <br>
+                        10th<br>
+                        <br>
+                        11th<br>
+                        <br>
+                        12th<br>
+                        <br>
+                        13th<br>
+                        <br>
+                        14th<br>
+                        <br>
+                        15th<br>
+                        <br>
+                        16th<br>
+                        <br>
+                        17th<br>
+                        <br>
+                        18th<br>
+                        <br>
+                        19th<br>
+                        <br>
+                        20th<br>
+                        <br>
+                        21st<br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
