2f03899b83845e18cf2af2205f9c1dbfc6a94f47
Kirby-b <kirby.brodie66@gmail.com>
2023-12-05 10:13:44 -0500

    Create 17th-Century.blade.php
    Added facts as well

---
+++ LearnX/resources/views/lessons/History/American/17th-Century.blade.php
@@ -0,0 +1,26 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                    17th Century:<br>
+                    <br>
+                    1607: Establishment of Jamestown, the first permanent English settlement.<br>
+                    1620: Signing of the Mayflower Compact by Pilgrims.<br>
+                    1630: The Puritans establish the Massachusetts Bay Colony.<br>
+                    1634: Founding of the Maryland colony by Lord Baltimore.<br>
+                    1675-1676: King Philip's War between Native Americans and New England colonists.<br>
+                    1681: William Penn receives the charter for Pennsylvania.<br>
+                    1692: Salem witch trials in Massachusetts.<br>
+                    1699: Founding of Williamsburg, Virginia's colonial capital.<br>
+                    1699: Establishment of the College of William & Mary.<br>
+                    1699: French settlers establish Mobile, the first capital of French Louisiana.<br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
