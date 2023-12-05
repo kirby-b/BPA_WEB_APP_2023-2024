6dad0aa0487901c63312739cf559ae9373ab5038
Kirby-b <kirby.brodie66@gmail.com>
2023-12-05 10:14:36 -0500

    Create Branches-Of-Government.blade.php
    Added facts as well

---
+++ LearnX/resources/views/lessons/History/Branches-Of-Government.blade.php
@@ -0,0 +1,25 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        Executive Branch(President):<br>
+                        Job: Enforces laws, manages the government, deals with other countries.<br>
+                        Power: Can veto laws from Congress.<br>
+                        <br>
+                        Legislative Branch(Congress):<br>
+                        Job: Makes laws, controls money, decides on war, approves presidential appointments.<br>
+                        Parts: Senate and House of Representatives.<br>
+                        <br>
+                        Judicial Branch(Courts):<br>
+                        Job: Interprets laws, checks if laws follow the Constitution.<br>
+                        Power: Can say if laws or actions are against the rules(unconstitutional).
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
