d8602926916d0fe9039ac4a241ed6ead6b3618df
Kirby-b <kirby.brodie66@gmail.com>
2023-12-05 10:13:38 -0500

    Create 16th-Century.blade.php
    Added facts as well

---
+++ LearnX/resources/views/lessons/History/American/16th-Century.blade.php
@@ -0,0 +1,27 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                    16th Century:<br>
+                    <br>
+                    1501: The first African slaves are brought to the Americas by the Spanish.<br>
+                    1503: The encomienda system is established.<br>
+                    1513: Juan Ponce de León explores Florida, the first known European expedition to the area.<br>
+                    1519-1521: Hernán Cortés conquers the Aztec Empire in Mexico.<br>
+                    1533: Francisco Pizarro conquers the Inca Empire in Peru.<br>
+                    1540-1542: Francisco Vázquez de Coronado explores the American Southwest.<br>
+                    1565: St. Augustine, Florida, is established, the oldest continuously inhabited European-established settlement in the continental United States.<br>
+                    1585: Roanoke Colony, the first English attempt at settlement in the Americas, is founded.<br>
+                    1588: The Spanish Armada is defeated by the English navy.<br>
+                    1590: The Roanoke Colony mysteriously disappears.<br>
+                    
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
