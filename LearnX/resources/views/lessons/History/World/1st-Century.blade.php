b05039914b7bd2fb6e7727e04f241da5a55f803a
Kirby-b <kirby.brodie66@gmail.com>
2023-12-05 10:17:57 -0500

    Create 1st-Century.blade.php
    added facts as well

---
+++ LearnX/resources/views/lessons/History/World/1st-Century.blade.php
@@ -0,0 +1,28 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>1st Century</b>
+                        </div>
+                        <br>                        
+                        206 BCE - 220 CE Establishment of the Han Dynasty in China<br>
+                        27 BCE-14 CE Formation of the Roman Empire<br>
+                        31 CE Fall of Sejanus<br>
+                        70-80 CE Construction of the Colosseum<br>
+                        70 CE Destruction of the Second Temple<br>
+                        79 CE Pompeii and Herculaneum<br>
+                        77-79 CE Publication of "Naturalis Historia" by Pliny the Elder<br>
+                        60-61 CE Boudica's Rebellion<br>
+                        64 CE The Great Fire of Rome<br>
+                        50 CE Council of Jerusalem<br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
