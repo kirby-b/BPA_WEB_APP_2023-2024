6c406fbf2a9a63ac4665001477cc927c15c93b02
Kirby-b <kirby.brodie66@gmail.com>
2023-12-05 10:13:50 -0500

    Create 18th-Century.blade.php
    Added facts as well

---
+++ LearnX/resources/views/lessons/History/American/18th-Century.blade.php
@@ -0,0 +1,26 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                    18th Century:<br>
+                    <br>
+                    1733: Founding of Savannah, Georgia, the last of the original 13 colonies.<br>
+                    1754-1763: The French and Indian War.<br>
+                    1770: The Boston Massacre.<br>
+                    1773: The Boston Tea Party.<br>
+                    1775-1783: The American Revolutionary War.<br>
+                    1787: Signing of the U.S. Constitution.<br>
+                    1789: George Washington becomes the first President of the United States.<br>
+                    1793: Invention of the cotton gin by Eli Whitney.<br>
+                    1796: Tennessee becomes the 16th state to join the Union.<br>
+                    1799: George Washington's death.<br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
