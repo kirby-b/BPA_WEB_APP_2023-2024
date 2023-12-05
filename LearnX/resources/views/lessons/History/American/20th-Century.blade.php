45a07fbf7f8b06812bbfc4111d192d17bd75bfa8
Kirby-b <kirby.brodie66@gmail.com>
2023-12-05 10:14:01 -0500

    Create 20th-Century.blade.php
    Added facts as well

---
+++ LearnX/resources/views/lessons/History/American/20th-Century.blade.php
@@ -0,0 +1,27 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                    20th Century:<br>
+                    <br>
+                    1913: The 17th Amendment is ratified, allowing for the direct election of senators.<br>
+                    1917-1918: The United States enters World War I.<br>
+                    1920: The 19th Amendment grants women the right to vote.<br>
+                    1929: The Great Depression begins.<br>
+                    1941: The attack on Pearl Harbor leads the U.S. to enter World War II.<br>
+                    1954: Brown v. Board of Education desegregates public schools.<br>
+                    1963: Martin Luther King Jr.'s "I Have a Dream" speech during the March on Washington.<br>
+                    1969: Apollo 11 lands the first humans on the moon.<br>
+                    1974: Richard Nixon resigns in the wake of the Watergate scandal.<br>
+                    1989: The fall of the Berlin Wall marks the end of the Cold War.<br>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
