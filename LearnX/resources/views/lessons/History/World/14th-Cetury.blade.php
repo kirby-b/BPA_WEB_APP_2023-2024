83bcaa5b7c5fcdca525778c276ec7c86f11af3e0
Kirby-b <kirby.brodie66@gmail.com>
2023-12-06 09:53:16 -0500

    Added 14th and 15th Century

---
+++ LearnX/resources/views/lessons/History/World/14th-Cetury.blade.php
@@ -0,0 +1,28 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>14th Century</b>
+                        </div>
+                        <br>
+                        1305-1378: Avignon Papacy.
+                        1315-1317: Great Famine in Europe.
+                        1337-1453: Hundred Years' War.
+                        1340: Battle of Sluys in the Hundred Years' War.
+                        1346: Battle of Crécy - English victory in the Hundred Years' War.
+                        1347-1351: Black Death pandemic.
+                        1356: Battle of Poitiers - English capture King John II of France.
+                        1368-1644: Ming Dynasty in China.
+                        1378-1417: Western Schism in the Catholic Church.
+                        1386-1389: Ottoman victory at the Battle of Kosovo.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
---
+++ LearnX/resources/views/lessons/History/World/15th-Century.blade.php
@@ -0,0 +1,28 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>15th Century</b>
+                        </div>
+                        <br>
+                        1405-1433: Zheng He's voyages across the Indian Ocean.
+                        1431: Joan of Arc is executed.
+                        1450-1455: Gutenberg prints the first Bible using movable type.
+                        1453: Fall of Constantinople to the Ottoman Empire.
+                        1478-1834: Spanish Inquisition is established.
+                        1485: Battle of Bosworth Field - End of the Wars of the Roses.
+                        1492: Reconquista concludes with the capture of Granada.
+                        1492: Christopher Columbus reaches the Americas.
+                        1492: The Alhambra Decree expels Jews from Spain.
+                        1498: Vasco da Gama reaches the shores of India.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
