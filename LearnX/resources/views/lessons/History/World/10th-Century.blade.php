0d1ff1df7f99b5774bd1b443ecb99b075ae70baa
Kirby-b <kirby.brodie66@gmail.com>
2023-12-06 09:52:48 -0500

    Added 10th and 11th Century

---
+++ LearnX/resources/views/lessons/History/World/10th-Century.blade.php
@@ -0,0 +1,28 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>10th Century</b>
+                        </div>
+                        <br>
+                        907-960: Period of the Five Dynasties and Ten Kingdoms in China.
+                        909: Fatimid Caliphate is founded in North Africa.
+                        945: Abbasid Caliphate recovers Baghdad from the Buyids.
+                        960-1279: Song Dynasty in China.
+                        962: Otto I is crowned Holy Roman Emperor.
+                        976-1031: Reign of Al-Hakam II in Cordoba's Golden Age.
+                        980-1016: Reign of Vladimir the Great in Kievan Rus'.
+                        988: Christianization of Kievan Rus' under Vladimir the Great.
+                        991: Battle of Maldon between the English and Vikings.
+                        1002-1014: Æthelred the Unready's tribute payments to the Vikings.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
---
+++ LearnX/resources/views/lessons/History/World/11th-Century.blade.php
@@ -0,0 +1,28 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>11th Century</b>
+                        </div>
+                        <br>
+                        1000-1100: Medieval Warm Period.
+                        1030: Death of Olaf II of Norway at the Battle of Stiklestad.
+                        1037-1194: Almoravid Empire in North Africa and Al-Andalus.
+                        1054: Great Schism between the Eastern Orthodox and Roman Catholic Churches.
+                        1066: Battle of Hastings - Norman Conquest of England.
+                        1071: Battle of Manzikert - Seljuk Turks defeat the Byzantine Empire.
+                        1073-1085: Reign of Pope Gregory VII.
+                        1086: Battle of Sagrajas - Almoravids defeat Castile and Leon.
+                        1095: Pope Urban II calls for the First Crusade at the Council of Clermont.
+                        1096-1099: First Crusade.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
