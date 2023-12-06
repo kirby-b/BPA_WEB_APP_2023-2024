b3ebb8bdc7b8e7a382930a6e254e9cf1be889f01
Kirby-b <kirby.brodie66@gmail.com>
2023-12-06 09:53:01 -0500

    Added 12th and 13th Century

---
+++ LearnX/resources/views/lessons/History/World/12th-Century.blade.php
@@ -0,0 +1,28 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>12th Century</b>
+                        </div>
+                        <br>
+                        1122: Concordat of Worms between the Holy Roman Empire and the Catholic Church.
+                        1139: Second Lateran Council.
+                        1145-1149: Wendish Crusade in Northern Europe.
+                        1147-1149: Second Crusade.
+                        1154-1189: Reign of Henry II of England
+                        1170: Murder of Thomas Becket in Canterbury Cathedral.
+                        1171: Saladin conquers Jerusalem.
+                        1187: Battle of Hattin - Saladin defeats the Crusaders.
+                        1189-1192: Third Crusade.
+                        1192: Treaty of Jaffa ends the Third Crusade.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
---
+++ LearnX/resources/views/lessons/History/World/13th-Century.blade.php
@@ -0,0 +1,28 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>13th Century</b>
+                        </div>
+                        <br>
+                        1202-1204: Fourth Crusade.
+                        1206: Genghis Khan unites the Mongol tribes.
+                        1206-1368: Mongol Yuan Dynasty in China.
+                        1215: Magna Carta is sealed by King John of England.
+                        1219-1221: Mongol invasion of Khwarazm.
+                        1227: Death of Genghis Khan.
+                        1237-1242: Mongol invasion of Europe - Battle of Legnica.
+                        1260: Battle of Ain Jalut - Mamluks defeat the Mongols.
+                        1271-1295: Marco Polo travels to China.
+                        1291: Fall of Acre - End of the Crusader states.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
