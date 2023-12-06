03bfd25491f27f2482a0e478be17fc2e9c6e2134
Kirby-b <kirby.brodie66@gmail.com>
2023-12-06 09:53:28 -0500

    Added 16th and 17th Century

---
+++ LearnX/resources/views/lessons/History/World/16th-Century.blade.php
@@ -0,0 +1,28 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>16th Century</b>
+                        </div>
+                        <br>
+                        1517: Martin Luther's Ninety-Five Theses initiate the Protestant Reformation.
+                        1521: Cortés conquers the Aztec Empire.
+                        1556-1605: Akbar the Great rules the Mughal Empire.
+                        1562-1598: French Wars of Religion.
+                        1571: Battle of Lepanto - Holy League defeats the Ottoman Empire.
+                        1588: Defeat of the Spanish Armada by the English navy.
+                        1590: Battle of Ivry - Henry IV becomes King of France.
+                        1598: Edict of Nantes grants religious tolerance in France.
+                        1599: Peace of Vervins ends the war between Spain and France.
+                        1600: Battle of Sekigahara unifies Japan under the Tokugawa shogunate.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
---
+++ LearnX/resources/views/lessons/History/World/17th-Century.blade.php
@@ -0,0 +1,28 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>17th Century</b>
+                        </div>
+                        <br>
+                        1602 Establishment of the Dutch East India Company:
+                        1618-1648 Thirty Years' War
+                        1642-1651 English Civil War
+                        1648 Peace of Westphalia
+                        1649 Execution of Charles I
+                        1659 Treaty of Treaty of Pyrenees
+                        1660 Establishment of the Royal Society
+                        1688 Glorious Revolution
+                        1692 Salem Witch Trials
+                        1713 Treaty of Utrecht
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
