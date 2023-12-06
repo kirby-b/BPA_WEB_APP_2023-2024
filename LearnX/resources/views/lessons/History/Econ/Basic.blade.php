d9682363b221786f6e15fbbeb261e7d637b0ec64
Kirby-b <kirby.brodie66@gmail.com>
2023-12-06 10:19:41 -0500

    Create Basic.blade.php

---
+++ LearnX/resources/views/lessons/History/Econ/Basic.blade.php
@@ -0,0 +1,25 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        Economics<br>
+                        <br>
+                        Basic<br>
+                        <br>
+                        <br>
+                        Scarcity<br>
+                        <br>
+                        Choice & Opprotunity Costs<br>
+                        <br>
+                        Supply & Demand<br>
+                        <!--Link all-->
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
