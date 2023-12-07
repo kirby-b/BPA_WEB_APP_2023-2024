c97508eb963a6da2ecf1babc623d06862d30c4b9
kirby-b <kirby.brodie66@gmail.com>
2023-12-07 16:39:09 -0500

    Econ Links

--- LearnX/resources/views/lessons/History/Econ/Personal-Finance.blade.php
+++ LearnX/resources/views/lessons/History/Econ/Personal-Finance.blade.php
@@ -9,20 +9,21 @@
                         Economy:<br>
                     </p>
                     <p class="text-xl font-sans m-4">
+                        <a href="{{ route('page.basic') }}" class="hover:text-purple-300">Basic</a><br>
                         <br>
-                        <a href="{{ route('page.century15.america') }}" class="hover:text-purple-300">15th Century</a><br>
+                        <a href="{{ route('page.typesOfEcon') }}" class="hover:text-purple-300">Types of Economy</a><br>
                         <br>
-                        <a href="{{ route('page.century16.america') }}" class="hover:text-purple-300">16th Century</a><br>
+                        <a href="{{ route('page.econIndicators') }}" class="hover:text-purple-300">Economy Indicators</a><br>
                         <br>
-                        <a href="{{ route('page.century17.america') }}" class="hover:text-purple-300">17th Century</a><br>
+                        <a href="{{ route('page.laborMarket') }}" class="hover:text-purple-300">Labor Market</a><br>
                         <br>
-                        <a href="{{ route('page.century18.america') }}" class="hover:text-purple-300">18th Century</a><br>
+                        <a href="{{ route('page.roleOfGov') }}" class="hover:text-purple-300">Role of the Government</a><br>
                         <br>
-                        <a href="{{ route('page.century19.america') }}" class="hover:text-purple-300">19th Century</a><br>
+                        <a href="{{ route('page.internatTrade') }}" class="hover:text-purple-300">International Trade</a><br>
                         <br>
-                        <a href="{{ route('page.century20.america') }}" class="hover:text-purple-300">20th Century</a><br>
+                        <a href="{{ route('page.personalFinance') }}" class="hover:text-purple-300">Personal Finace</a><br>
                         <br>
-                        <a href="{{ route('page.century21.america') }}" class="hover:text-purple-300">21st Century</a><br>
+                        <a href="{{ route('page.marketStructures') }}" class="hover:text-purple-300">Market Structures</a><br>
                     </p>
                 </div>
                 <!-- Text Section -->
