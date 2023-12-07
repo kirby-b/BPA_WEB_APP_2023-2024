6f2f723f189efb14c1163158378e08a69412c2ab
Kirby-b <kirby.brodie66@gmail.com>
2023-12-07 08:38:56 -0500

    Update 19th-Century.blade.php

--- LearnX/resources/views/lessons/History/World/19th-Century.blade.php
+++ LearnX/resources/views/lessons/History/World/19th-Century.blade.php
@@ -2,24 +2,73 @@
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
             <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
-
+                <!-- Links Section -->
+                <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                        World History:<br>
+                    </p>
+                    <p class="text-lg font-sans m-4">
+                        <a href="{{ route('page.century1.world') }}" class="hover:text-purple-300">1st Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century2.world') }}" class="hover:text-purple-300">2nd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century3.world') }}" class="hover:text-purple-300">3rd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century4.world') }}" class="hover:text-purple-300">4th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century5.world') }}" class="hover:text-purple-300">5th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century6.world') }}" class="hover:text-purple-300">6th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century7.world') }}" class="hover:text-purple-300">7th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century8.world') }}" class="hover:text-purple-300">8th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century9.world') }}" class="hover:text-purple-300">9th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century10.world') }}" class="hover:text-purple-300">10th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century11.world') }}" class="hover:text-purple-300">11th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century12.world') }}" class="hover:text-purple-300">12th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century13.world') }}" class="hover:text-purple-300">13th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century14.world') }}" class="hover:text-purple-300">14th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century15.world') }}" class="hover:text-purple-300">15th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century16.world') }}" class="hover:text-purple-300">16th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century17.world') }}" class="hover:text-purple-300">17th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century18.world') }}" class="hover:text-purple-300">18th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century19.world') }}" class="hover:text-purple-300">19th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century20.world') }}" class="hover:text-purple-300">20th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century21.world') }}" class="hover:text-purple-300">21st Century</a><br>
+                    </p>
+                </div>
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
                         <div class="font-Preahvihear items-center text-2xl">
-                            <b>17th Century</b>
+                            <b>19th Century</b>
                         </div>
                         <br>
-                        1803: Louisiana Purchase in the United States.
-                        1804-1814: Haitian Revolution.
-                        1815: Battle of Waterloo - End of the Napoleonic Wars.
-                        1830: Revolutions of 1830 in Europe.
-                        1848: Revolutions of 1848 in Europe.
-                        1859: Publication of Charles Darwin's "On the Origin of Species."
-                        1861-1865: American Civil War.
-                        1868: Meiji Restoration in Japan.
-                        1871: Unification of Germany.
-                        1898: Spanish-American War.
+                        1803: Louisiana Purchase in the United States.<br>
+                        1804-1814: Haitian Revolution.<br>
+                        1815: Battle of Waterloo - End of the Napoleonic Wars.<br>
+                        1830: Revolutions of 1830 in Europe.<br>
+                        1848: Revolutions of 1848 in Europe.<br>
+                        1859: Publication of Charles Darwin's "On the Origin of Species."<br>
+                        1861-1865: American Civil War.<br>
+                        1868: Meiji Restoration in Japan.<br>
+                        1871: Unification of Germany.<br>
+                        1898: Spanish-American War.<br>
                     </p>
                 </div>
             </div>
