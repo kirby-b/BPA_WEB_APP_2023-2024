7fa2dc4d3a74b6b7032c5652aba1289f94c7ce5d
Kirby-b <kirby.brodie66@gmail.com>
2023-12-07 08:37:44 -0500

    Update 11th-Century.blade.php

--- LearnX/resources/views/lessons/History/World/11th-Century.blade.php
+++ LearnX/resources/views/lessons/History/World/11th-Century.blade.php
@@ -2,7 +2,56 @@
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
             <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
-
+                <!-- Links Section -->
+                <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                        World History:<br>
+                    </p>
+                    <p class="text-lg font-sans m-4">
+                        <a href="{{ route('page.century1.world') }}" class="hover:text-purple-300">1st Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century2.world') }}" class="hover:text-purple-300">2nd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century3.world') }}" class="hover:text-purple-300">3rd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century4.world') }}" class="hover:text-purple-300">4th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century5.world') }}" class="hover:text-purple-300">5th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century6.world') }}" class="hover:text-purple-300">6th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century7.world') }}" class="hover:text-purple-300">7th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century8.world') }}" class="hover:text-purple-300">8th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century9.world') }}" class="hover:text-purple-300">9th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century10.world') }}" class="hover:text-purple-300">10th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century11.world') }}" class="hover:text-purple-300">11th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century12.world') }}" class="hover:text-purple-300">12th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century13.world') }}" class="hover:text-purple-300">13th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century14.world') }}" class="hover:text-purple-300">14th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century15.world') }}" class="hover:text-purple-300">15th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century16.world') }}" class="hover:text-purple-300">16th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century17.world') }}" class="hover:text-purple-300">17th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century18.world') }}" class="hover:text-purple-300">18th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century19.world') }}" class="hover:text-purple-300">19th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century20.world') }}" class="hover:text-purple-300">20th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century21.world') }}" class="hover:text-purple-300">21st Century</a><br>
+                    </p>
+                </div>
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
@@ -10,16 +59,16 @@
                             <b>11th Century</b>
                         </div>
                         <br>
-                        1000-1100: Medieval Warm Period.
-                        1030: Death of Olaf II of Norway at the Battle of Stiklestad.
-                        1037-1194: Almoravid Empire in North Africa and Al-Andalus.
-                        1054: Great Schism between the Eastern Orthodox and Roman Catholic Churches.
-                        1066: Battle of Hastings - Norman Conquest of England.
-                        1071: Battle of Manzikert - Seljuk Turks defeat the Byzantine Empire.
-                        1073-1085: Reign of Pope Gregory VII.
-                        1086: Battle of Sagrajas - Almoravids defeat Castile and Leon.
-                        1095: Pope Urban II calls for the First Crusade at the Council of Clermont.
-                        1096-1099: First Crusade.
+                        1000-1100: Medieval Warm Period.<br>
+                        1030: Death of Olaf II of Norway at the Battle of Stiklestad.<br>
+                        1037-1194: Almoravid Empire in North Africa and Al-Andalus.<br>
+                        1054: Great Schism between the Eastern Orthodox and Roman Catholic Churches.<br>
+                        1066: Battle of Hastings - Norman Conquest of England.<br>
+                        1071: Battle of Manzikert - Seljuk Turks defeat the Byzantine Empire.<br>
+                        1073-1085: Reign of Pope Gregory VII.<br>
+                        1086: Battle of Sagrajas - Almoravids defeat Castile and Leon.<br>
+                        1095: Pope Urban II calls for the First Crusade at the Council of Clermont.<br>
+                        1096-1099: First Crusade.<br>
                     </p>
                 </div>
             </div>
