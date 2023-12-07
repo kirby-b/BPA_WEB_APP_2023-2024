c65382ba98a22fe97118c11017e397c3ece326aa
kirby-b <kirby.brodie66@gmail.com>
2023-12-07 17:09:47 -0500

    Update Definitions.blade.php

--- LearnX/resources/views/lessons/English/Definitions.blade.php
+++ LearnX/resources/views/lessons/English/Definitions.blade.php
@@ -37,10 +37,21 @@
                         <div class="font-Preahvihear items-center text-2xl">
                             <b>DEFINITIONS</b>
                         </div>
-        
-                        <!--non-nouns
-                        
-                        ideas-->
+                        Ocean:A massive body of salt water.<br>
+                        Law: A rule written for society to keep it in order.<br>
+                        Election: When a society decides who is the leader.<br>
+                        Hieroglyphic: A form of language written with small pictures and symbols meant to represent sounds and words.<br>
+                        Civilization: A society that has become advanced in one way or another.<br>
+                        Agriculture: Harvesting and growing plants and/or animals for food and other needs.<br>
+                        Astronomy: The study of stars and planets.<br>
+                        Hierarchy: The social order.One group will be on top and another will be on the bottom.<br>
+                        Colosseum: A large theater. Used as a place of entertainment.<br>
+                        Dynasty: A line of leaders who are related to each other.<br>
+                        Plague: A virus, bacteria, or other such thing that causes massive amounts of harm and death.<br>
+                        Revolution: When a society decides to over throw something and replace it(usually a leader)<br>
+                        Treaty: When a group of diplomats sit down and make a deal for a collective goal.<br>
+                        Crisis: A very ergent and harmful thing/time.<br>
+                        Settlement: When a group of people move somewhere else and build a new society.<br>
                     </p>
                 </div>
             </div>
