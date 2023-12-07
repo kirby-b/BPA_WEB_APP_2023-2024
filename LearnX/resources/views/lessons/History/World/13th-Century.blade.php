bc4f39020622d0d0a759b2e40b1ace3bf15eb132
Kirby-b <kirby.brodie66@gmail.com>
2023-12-07 08:37:54 -0500

    Update 13th-Century.blade.php

--- LearnX/resources/views/lessons/History/World/13th-Century.blade.php
+++ LearnX/resources/views/lessons/History/World/13th-Century.blade.php
@@ -2,7 +2,56 @@
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
             <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
-
+                <!-- Links Section -->
+                <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                        World History:<br>
+                    </p>
+                    <p class="text-lg font-sans m-4">
+                        <a href="{{ route('page.century1.world') }}" class="hover:text-purple-300">1st Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century2.world') }}" class="hover:text-purple-300">2nd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century3.world') }}" class="hover:text-purple-300">3rd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century4.world') }}" class="hover:text-purple-300">4th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century5.world') }}" class="hover:text-purple-300">5th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century6.world') }}" class="hover:text-purple-300">6th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century7.world') }}" class="hover:text-purple-300">7th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century8.world') }}" class="hover:text-purple-300">8th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century9.world') }}" class="hover:text-purple-300">9th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century10.world') }}" class="hover:text-purple-300">10th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century11.world') }}" class="hover:text-purple-300">11th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century12.world') }}" class="hover:text-purple-300">12th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century13.world') }}" class="hover:text-purple-300">13th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century14.world') }}" class="hover:text-purple-300">14th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century15.world') }}" class="hover:text-purple-300">15th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century16.world') }}" class="hover:text-purple-300">16th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century17.world') }}" class="hover:text-purple-300">17th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century18.world') }}" class="hover:text-purple-300">18th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century19.world') }}" class="hover:text-purple-300">19th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century20.world') }}" class="hover:text-purple-300">20th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century21.world') }}" class="hover:text-purple-300">21st Century</a><br>
+                    </p>
+                </div>
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
@@ -10,16 +59,16 @@
                             <b>13th Century</b>
                         </div>
                         <br>
-                        1202-1204: Fourth Crusade.
-                        1206: Genghis Khan unites the Mongol tribes.
-                        1206-1368: Mongol Yuan Dynasty in China.
-                        1215: Magna Carta is sealed by King John of England.
-                        1219-1221: Mongol invasion of Khwarazm.
-                        1227: Death of Genghis Khan.
-                        1237-1242: Mongol invasion of Europe - Battle of Legnica.
-                        1260: Battle of Ain Jalut - Mamluks defeat the Mongols.
-                        1271-1295: Marco Polo travels to China.
-                        1291: Fall of Acre - End of the Crusader states.
+                        1202-1204: Fourth Crusade.<br>
+                        1206: Genghis Khan unites the Mongol tribes.<br>
+                        1206-1368: Mongol Yuan Dynasty in China.<br>
+                        1215: Magna Carta is sealed by King John of England.<br>
+                        1219-1221: Mongol invasion of Khwarazm.<br>
+                        1227: Death of Genghis Khan.<br>
+                        1237-1242: Mongol invasion of Europe - Battle of Legnica.<br>
+                        1260: Battle of Ain Jalut - Mamluks defeat the Mongols.<br>
+                        1271-1295: Marco Polo travels to China.<br>
+                        1291: Fall of Acre - End of the Crusader states.<br>
                     </p>
                 </div>
             </div>
