55209f6db6581120333809e6527a7ca992e76017
Kirby-b <kirby.brodie66@gmail.com>
2023-12-07 08:37:46 -0500

    Update 12th-Century.blade.php

--- LearnX/resources/views/lessons/History/World/12th-Century.blade.php
+++ LearnX/resources/views/lessons/History/World/12th-Century.blade.php
@@ -2,7 +2,56 @@
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
             <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
-
+                <!-- Links Section -->
+                <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                        World History:<br>
+                    </p>
+                    <p class="text-lg font-sans m-4">
+                        <a href="{{ route('page.century1.world') }}" class="hover:text-purple-300">1st Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century2.world') }}" class="hover:text-purple-300">2nd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century3.world') }}" class="hover:text-purple-300">3rd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century4.world') }}" class="hover:text-purple-300">4th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century5.world') }}" class="hover:text-purple-300">5th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century6.world') }}" class="hover:text-purple-300">6th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century7.world') }}" class="hover:text-purple-300">7th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century8.world') }}" class="hover:text-purple-300">8th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century9.world') }}" class="hover:text-purple-300">9th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century10.world') }}" class="hover:text-purple-300">10th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century11.world') }}" class="hover:text-purple-300">11th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century12.world') }}" class="hover:text-purple-300">12th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century13.world') }}" class="hover:text-purple-300">13th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century14.world') }}" class="hover:text-purple-300">14th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century15.world') }}" class="hover:text-purple-300">15th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century16.world') }}" class="hover:text-purple-300">16th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century17.world') }}" class="hover:text-purple-300">17th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century18.world') }}" class="hover:text-purple-300">18th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century19.world') }}" class="hover:text-purple-300">19th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century20.world') }}" class="hover:text-purple-300">20th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century21.world') }}" class="hover:text-purple-300">21st Century</a><br>
+                    </p>
+                </div>
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
@@ -10,16 +59,16 @@
                             <b>12th Century</b>
                         </div>
                         <br>
-                        1122: Concordat of Worms between the Holy Roman Empire and the Catholic Church.
-                        1139: Second Lateran Council.
-                        1145-1149: Wendish Crusade in Northern Europe.
-                        1147-1149: Second Crusade.
-                        1154-1189: Reign of Henry II of England
-                        1170: Murder of Thomas Becket in Canterbury Cathedral.
-                        1171: Saladin conquers Jerusalem.
-                        1187: Battle of Hattin - Saladin defeats the Crusaders.
-                        1189-1192: Third Crusade.
-                        1192: Treaty of Jaffa ends the Third Crusade.
+                        1122: Concordat of Worms between the Holy Roman Empire and the Catholic Church.<br>
+                        1139: Second Lateran Council.<br>
+                        1145-1149: Wendish Crusade in Northern Europe.<br>
+                        1147-1149: Second Crusade.<br>
+                        1154-1189: Reign of Henry II of England.<br>
+                        1170: Murder of Thomas Becket in Canterbury Cathedral.<br>
+                        1171: Saladin conquers Jerusalem.<br>
+                        1187: Battle of Hattin - Saladin defeats the Crusaders.<br>
+                        1189-1192: Third Crusade.<br>
+                        1192: Treaty of Jaffa ends the Third Crusade.<br>
                     </p>
                 </div>
             </div>
