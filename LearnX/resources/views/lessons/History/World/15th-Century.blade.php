3add4043d56a8489dcb3a503e87ee1c73c0cc9e5
Kirby-b <kirby.brodie66@gmail.com>
2023-12-07 08:38:20 -0500

    Update 15th-Century.blade.php

--- LearnX/resources/views/lessons/History/World/15th-Century.blade.php
+++ LearnX/resources/views/lessons/History/World/15th-Century.blade.php
@@ -2,7 +2,56 @@
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
             <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
-
+                <!-- Links Section -->
+                <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                        World History:<br>
+                    </p>
+                    <p class="text-lg font-sans m-4">
+                        <a href="{{ route('page.century1.world') }}" class="hover:text-purple-300">1st Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century2.world') }}" class="hover:text-purple-300">2nd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century3.world') }}" class="hover:text-purple-300">3rd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century4.world') }}" class="hover:text-purple-300">4th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century5.world') }}" class="hover:text-purple-300">5th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century6.world') }}" class="hover:text-purple-300">6th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century7.world') }}" class="hover:text-purple-300">7th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century8.world') }}" class="hover:text-purple-300">8th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century9.world') }}" class="hover:text-purple-300">9th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century10.world') }}" class="hover:text-purple-300">10th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century11.world') }}" class="hover:text-purple-300">11th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century12.world') }}" class="hover:text-purple-300">12th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century13.world') }}" class="hover:text-purple-300">13th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century14.world') }}" class="hover:text-purple-300">14th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century15.world') }}" class="hover:text-purple-300">15th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century16.world') }}" class="hover:text-purple-300">16th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century17.world') }}" class="hover:text-purple-300">17th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century18.world') }}" class="hover:text-purple-300">18th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century19.world') }}" class="hover:text-purple-300">19th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century20.world') }}" class="hover:text-purple-300">20th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century21.world') }}" class="hover:text-purple-300">21st Century</a><br>
+                    </p>
+                </div>
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
@@ -10,16 +59,16 @@
                             <b>15th Century</b>
                         </div>
                         <br>
-                        1405-1433: Zheng He's voyages across the Indian Ocean.
-                        1431: Joan of Arc is executed.
-                        1450-1455: Gutenberg prints the first Bible using movable type.
-                        1453: Fall of Constantinople to the Ottoman Empire.
-                        1478-1834: Spanish Inquisition is established.
-                        1485: Battle of Bosworth Field - End of the Wars of the Roses.
-                        1492: Reconquista concludes with the capture of Granada.
-                        1492: Christopher Columbus reaches the Americas.
-                        1492: The Alhambra Decree expels Jews from Spain.
-                        1498: Vasco da Gama reaches the shores of India.
+                        1405-1433: Zheng He's voyages across the Indian Ocean.<br>
+                        1431: Joan of Arc is executed.<br>
+                        1450-1455: Gutenberg prints the first Bible using movable type.<br>
+                        1453: Fall of Constantinople to the Ottoman Empire.<br>
+                        1478-1834: Spanish Inquisition is established.<br>
+                        1485: Battle of Bosworth Field - End of the Wars of the Roses.<br>
+                        1492: Reconquista concludes with the capture of Granada.<br>
+                        1492: Christopher Columbus reaches the Americas.<br>
+                        1492: The Alhambra Decree expels Jews from Spain.<br>
+                        1498: Vasco da Gama reaches the shores of India.<br>
                     </p>
                 </div>
             </div>
