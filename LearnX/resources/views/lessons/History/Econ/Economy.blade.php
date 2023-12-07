4f21e5605f307b07ef6f20751a65775e4f095692
kirby-b <kirby.brodie66@gmail.com>
2023-12-07 16:38:54 -0500

    History links

--- LearnX/resources/views/lessons/History/Econ/Economy.blade.php
+++ LearnX/resources/views/lessons/History/Econ/Economy.blade.php
@@ -8,20 +8,26 @@
                         History:<br>
                     </p>
                     <p class="text-xl font-sans m-4">
+                        <a href="{{ route('page.holidays') }}" class="hover:text-purple-300">Holidays Around the World</a>
                         <br>
-                        <a href="{{ route('page.century15.america') }}" class="hover:text-purple-300">15th Century</a><br>
+                        <a href="{{ route('page.contenents.oceans') }}" class="hover:text-purple-300">Contenents/Oceans</a>
                         <br>
-                        <a href="{{ route('page.century16.america') }}" class="hover:text-purple-300">16th Century</a><br>
+                        <a href="{{ route('page.states') }}" class="hover:text-purple-300">States in the U.S.</a>
                         <br>
-                        <a href="{{ route('page.century17.america') }}" class="hover:text-purple-300">17th Century</a><br>
+                        <a href="{{ route('page.branches.of.gov') }}" class="hover:text-purple-300">Branches of the U.S. Government</a>
                         <br>
-                        <a href="{{ route('page.century18.america') }}" class="hover:text-purple-300">18th Century</a><br>
+                        <a href="{{ route('page.ancient.history') }}" class="hover:text-purple-300">Ancient History</a>
                         <br>
-                        <a href="{{ route('page.century19.america') }}" class="hover:text-purple-300">19th Century</a><br>
+                        <a href="{{ route('page.american.history') }}" class="hover:text-purple-300">American History</a>
                         <br>
-                        <a href="{{ route('page.century20.america') }}" class="hover:text-purple-300">20th Century</a><br>
+                        <a href="{{ route('page.world.history') }}" class="hover:text-purple-300">World History</a>
                         <br>
-                        <a href="{{ route('page.century21.america') }}" class="hover:text-purple-300">21st Century</a><br>
+                        <a href="{{ route('page.economy') }}" class="hover:text-purple-300">Economics</a>
+                        <br>
+                        <a href="{{ route('page.civics') }}" class="hover:text-purple-300">Civics</a>
+                        <br>
+                        <a href="{{ route('page.politics') }}" class="hover:text-purple-300">Politics</a>
+                        <br>
                     </p>
                 </div>
                 <!-- Text Section -->
@@ -45,7 +51,6 @@
                         <a href="{{ route('page.personalFinance') }}" class="hover:text-purple-300">Personal Finace</a><br>
                         <br>
                         <a href="{{ route('page.marketStructures') }}" class="hover:text-purple-300">Market Structures</a><br>
-                        <!--Link all-->
                     </p>
                 </div>
             </div>
