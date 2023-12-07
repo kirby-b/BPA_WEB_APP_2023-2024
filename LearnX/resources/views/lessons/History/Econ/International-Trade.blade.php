46a5dd5b613ed1db770e581ee7946d7b7781c310
Kirby-b <kirby.brodie66@gmail.com>
2023-12-07 08:16:48 -0500

    Update International-Trade.blade.php

---
+++ LearnX/resources/views/lessons/History/Econ/International-Trade.blade.php
@@ -0,0 +1,24 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        Economics<br>
+                        <br>
+                        International Trade<br>
+                        <br>
+                        <br>
+                        Scarcity<br>
+                        <br>
+                        Choice & Opprotunity Costs<br>
+                        <br>
+                        Supply & Demand<br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
