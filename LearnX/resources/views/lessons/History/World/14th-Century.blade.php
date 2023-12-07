21837bb87695be3c620b9487befc08ec7352f013
Kirby-b <kirby.brodie66@gmail.com>
2023-12-07 08:38:15 -0500

    Renamed and added to 14th century

---
+++ LearnX/resources/views/lessons/History/World/14th-Century.blade.php
@@ -0,0 +1,77 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+                <!-- Links Section -->
+                <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                        World History:<br>
+                    </p>
+                    <p class="text-lg font-sans m-4">
+                        <a href="{{ route('page.century1.world') }}" class="hover:text-purple-300">1st Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century2.world') }}" class="hover:text-purple-300">2nd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century3.world') }}" class="hover:text-purple-300">3rd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century4.world') }}" class="hover:text-purple-300">4th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century5.world') }}" class="hover:text-purple-300">5th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century6.world') }}" class="hover:text-purple-300">6th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century7.world') }}" class="hover:text-purple-300">7th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century8.world') }}" class="hover:text-purple-300">8th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century9.world') }}" class="hover:text-purple-300">9th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century10.world') }}" class="hover:text-purple-300">10th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century11.world') }}" class="hover:text-purple-300">11th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century12.world') }}" class="hover:text-purple-300">12th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century13.world') }}" class="hover:text-purple-300">13th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century14.world') }}" class="hover:text-purple-300">14th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century15.world') }}" class="hover:text-purple-300">15th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century16.world') }}" class="hover:text-purple-300">16th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century17.world') }}" class="hover:text-purple-300">17th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century18.world') }}" class="hover:text-purple-300">18th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century19.world') }}" class="hover:text-purple-300">19th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century20.world') }}" class="hover:text-purple-300">20th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century21.world') }}" class="hover:text-purple-300">21st Century</a><br>
+                    </p>
+                </div>
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        <div class="font-Preahvihear items-center text-2xl">
+                            <b>14th Century</b>
+                        </div>
+                        <br>
+                        1305-1378: Avignon Papacy.<br>
+                        1315-1317: Great Famine in Europe.<br>
+                        1337-1453: Hundred Years' War.<br>
+                        1340: Battle of Sluys in the Hundred Years' War.<br>
+                        1346: Battle of Crécy - English victory in the Hundred Years' War.<br>
+                        1347-1351: Black Death pandemic.<br>
+                        1356: Battle of Poitiers - English capture King John II of France.<br>
+                        1368-1644: Ming Dynasty in China.<br>
+                        1378-1417: Western Schism in the Catholic Church.<br>
+                        1386-1389: Ottoman victory at the Battle of Kosovo.<br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
