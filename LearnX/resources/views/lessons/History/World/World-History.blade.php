08abc5bf7987423128de40d925c3ea6d09c5379c
Kirby-b <kirby.brodie66@gmail.com>
2023-12-07 08:17:11 -0500

    Update World-History.blade.php
    Links!!!

--- LearnX/resources/views/lessons/History/World/World-History.blade.php
+++ LearnX/resources/views/lessons/History/World/World-History.blade.php
@@ -5,48 +5,51 @@
 
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <div class="font-Preahvihear text-center items-center text-2xl">
+                        <b>World History</b>
+                    </div>
                     <p class="text-xl font-sans m-4">
-                        1st Century<br>
+                        <a href="{{ route('page.century1.world') }}" class="hover:text-purple-300">1st Century</a><br>
                         <br>
-                        2nd Century<br>
+                        <a href="{{ route('page.century2.world') }}" class="hover:text-purple-300">2nd Century</a><br>
                         <br>
-                        3rd Century<br>
+                        <a href="{{ route('page.century3.world') }}" class="hover:text-purple-300">3rd Century</a><br>
                         <br>
-                        4th Century<br>
+                        <a href="{{ route('page.century4.world') }}" class="hover:text-purple-300">4th Century</a><br>
                         <br>
-                        5th Century<br>
+                        <a href="{{ route('page.century5.world') }}" class="hover:text-purple-300">5th Century</a><br>
                         <br>
-                        6th Century<br>
+                        <a href="{{ route('page.century6.world') }}" class="hover:text-purple-300">6th Century</a><br>
                         <br>
-                        7th Century<br>
+                        <a href="{{ route('page.century7.world') }}" class="hover:text-purple-300">7th Century</a><br>
                         <br>
-                        8th Century<br>
+                        <a href="{{ route('page.century8.world') }}" class="hover:text-purple-300">8th Century</a><br>
                         <br>
-                        9th Century<br>
+                        <a href="{{ route('page.century9.world') }}" class="hover:text-purple-300">9th Century</a><br>
                         <br>
-                        10th Century<br>
+                        <a href="{{ route('page.century10.world') }}" class="hover:text-purple-300">10th Century</a><br>
                         <br>
-                        11th Century<br>
+                        <a href="{{ route('page.century11.world') }}" class="hover:text-purple-300">11th Century</a><br>
                         <br>
-                        12th Century<br>
+                        <a href="{{ route('page.century12.world') }}" class="hover:text-purple-300">12th Century</a><br>
                         <br>
-                        13th Century<br>
+                        <a href="{{ route('page.century13.world') }}" class="hover:text-purple-300">13th Century</a><br>
                         <br>
-                        14th Century<br>
+                        <a href="{{ route('page.century14.world') }}" class="hover:text-purple-300">14th Century</a><br>
                         <br>
-                        15th Century<br>
+                        <a href="{{ route('page.century15.world') }}" class="hover:text-purple-300">15th Century</a><br>
                         <br>
-                        16th Century<br>
+                        <a href="{{ route('page.century16.world') }}" class="hover:text-purple-300">16th Century</a><br>
                         <br>
-                        17th Century<br>
+                        <a href="{{ route('page.century17.world') }}" class="hover:text-purple-300">17th Century</a><br>
                         <br>
-                        18th Century<br>
+                        <a href="{{ route('page.century18.world') }}" class="hover:text-purple-300">18th Century</a><br>
                         <br>
-                        19th Century<br>
+                        <a href="{{ route('page.century19.world') }}" class="hover:text-purple-300">19th Century</a><br>
                         <br>
-                        20th Century<br>
+                        <a href="{{ route('page.century20.world') }}" class="hover:text-purple-300">20th Century</a><br>
                         <br>
-                        21st Century<br>
+                        <a href="{{ route('page.century21.world') }}" class="hover:text-purple-300">21st Century</a><br>
                     </p>
                 </div>
             </div>
