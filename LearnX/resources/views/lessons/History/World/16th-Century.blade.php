2b15f86c32c7e2c849e1ac61a3fd26f16d080fce
Kirby-b <kirby.brodie66@gmail.com>
2023-12-07 08:38:23 -0500

    Update 16th-Century.blade.php

--- LearnX/resources/views/lessons/History/World/16th-Century.blade.php
+++ LearnX/resources/views/lessons/History/World/16th-Century.blade.php
@@ -2,7 +2,56 @@
     <div class="bg-gray-100 py-10">
         <div class="container mx-auto px-4">
             <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
-
+                <!-- Links Section -->
+                <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                        World History:<br>
+                    </p>
+                    <p class="text-lg font-sans m-4">
+                        <a href="{{ route('page.century1.world') }}" class="hover:text-purple-300">1st Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century2.world') }}" class="hover:text-purple-300">2nd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century3.world') }}" class="hover:text-purple-300">3rd Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century4.world') }}" class="hover:text-purple-300">4th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century5.world') }}" class="hover:text-purple-300">5th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century6.world') }}" class="hover:text-purple-300">6th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century7.world') }}" class="hover:text-purple-300">7th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century8.world') }}" class="hover:text-purple-300">8th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century9.world') }}" class="hover:text-purple-300">9th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century10.world') }}" class="hover:text-purple-300">10th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century11.world') }}" class="hover:text-purple-300">11th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century12.world') }}" class="hover:text-purple-300">12th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century13.world') }}" class="hover:text-purple-300">13th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century14.world') }}" class="hover:text-purple-300">14th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century15.world') }}" class="hover:text-purple-300">15th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century16.world') }}" class="hover:text-purple-300">16th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century17.world') }}" class="hover:text-purple-300">17th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century18.world') }}" class="hover:text-purple-300">18th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century19.world') }}" class="hover:text-purple-300">19th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century20.world') }}" class="hover:text-purple-300">20th Century</a><br>
+                        <br>
+                        <a href="{{ route('page.century21.world') }}" class="hover:text-purple-300">21st Century</a><br>
+                    </p>
+                </div>
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
@@ -10,16 +59,16 @@
                             <b>16th Century</b>
                         </div>
                         <br>
-                        1517: Martin Luther's Ninety-Five Theses initiate the Protestant Reformation.
-                        1521: Cortés conquers the Aztec Empire.
-                        1556-1605: Akbar the Great rules the Mughal Empire.
-                        1562-1598: French Wars of Religion.
-                        1571: Battle of Lepanto - Holy League defeats the Ottoman Empire.
-                        1588: Defeat of the Spanish Armada by the English navy.
-                        1590: Battle of Ivry - Henry IV becomes King of France.
-                        1598: Edict of Nantes grants religious tolerance in France.
-                        1599: Peace of Vervins ends the war between Spain and France.
-                        1600: Battle of Sekigahara unifies Japan under the Tokugawa shogunate.
+                        1517: Martin Luther's Ninety-Five Theses initiate the Protestant Reformation.<br>
+                        1521: Cortés conquers the Aztec Empire.<br>
+                        1556-1605: Akbar the Great rules the Mughal Empire.<br>
+                        1562-1598: French Wars of Religion.<br>
+                        1571: Battle of Lepanto - Holy League defeats the Ottoman Empire.<br>
+                        1588: Defeat of the Spanish Armada by the English navy.<br>
+                        1590: Battle of Ivry - Henry IV becomes King of France.<br>
+                        1598: Edict of Nantes grants religious tolerance in France.<br>
+                        1599: Peace of Vervins ends the war between Spain and France.<br>
+                        1600: Battle of Sekigahara unifies Japan under the Tokugawa shogunate.<br>
                     </p>
                 </div>
             </div>
