8259c39d8ce13b1cb60fae4a8a28da4118548948
Kirby-b <kirby.brodie66@gmail.com>
2023-12-13 09:38:27 -0500

    Update DeleteAccount.blade.php

--- LearnX/resources/views/users/DeleteAccount.blade.php
+++ LearnX/resources/views/users/DeleteAccount.blade.php
@@ -9,15 +9,9 @@
                         <p class="text-xl mb-1">Account Termination</p>
                         <p class="text-base mb-1">To confirm please put in your name, username, and password</p>
                         <!--Goes to the authenticate route and then to the UserController-->
-                        <form method="POST" action="">
+                        <form method="POST" action="/delete">
                             @csrf <!--Makes it so people cant use scripts against your site-->
                             <div class="items-center">
-                                <div>
-                                    <input class="w-75 h-15 border-black border-solid border-2 p-3 text-lg" type="text" id="name" name="name" placeholder="Name" value="{{old('name')}}">
-                                    @error('name')
-                                        <p class="text-red-500 text-xs mt-2">{{$message}}</p>
-                                    @enderror
-                                </div>
                                 <div>
                                     <input class="w-75 h-15 border-black border-solid border-2 p-3 text-lg" type="text" id="email" name="email" placeholder="Email" value="{{old('email')}}">
                                     @error('email')
@@ -31,8 +25,9 @@
                                         <p class="text-red-500 text-xs mt-2">{{$message}}</p>
                                     @enderror
                                 </div>
+                                <br>
                                 <div class= "">
-                                    <input class="font-Preahvihear text-xl no-underline border-none cursor-pointer text-blue-600 hover:text-blue-800" type="submit" id="submit" name="submit" value="Lets Go >>>">
+                                    <input class="font-Preahvihear text-xl no-underline border-none cursor-pointer text-red-600 hover:text-blue-800" type="submit" id="submit" name="submit" value="Terminate Account">
                                 </div>
                                 <!--Gets your input and tries to verify your account info. endError sections determine what will happen if they input the
                                 wrong info -->
