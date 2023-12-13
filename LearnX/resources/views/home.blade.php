c3dce533981b859b3200206b7970fa701c9c6baf
Kirby-b <kirby.brodie66@gmail.com>
2023-12-13 09:37:31 -0500

    Update home.blade.php
    -Added delete account link

--- LearnX/resources/views/home.blade.php
+++ LearnX/resources/views/home.blade.php
@@ -1,9 +1,9 @@
 <x-partials.UserLayout>
-        <h1>This is temporarily how all views will be accessed. In time a way to render based on permissions will be used.</h1>
-        <form method="POST" action="/logout">
-            @csrf
-            <input class="text-black border-none no-underline cursor-pointer" type="submit" id="submit" name="submit" value="Logout">
-        </form>
-    </head>
+    <h1>This is temporarily how all views will be accessed. In time a way to render based on permissions will be used.</h1>
+    <form method="POST" action="/logout">
+        @csrf
+        <input class="text-black border-none no-underline cursor-pointer" type="submit" id="submit" name="submit" value="Logout">
+    </form>
+    <a class="no-underline text-black" type="button" value ="Yes" class href={{ route("page.accountTermination") }}>Delete Account</a> <br>
     <a class="no-underline text-black" type="button" value ="Yes" class href={{ route("page.student") }}>Student View</a> 
 </x-partials.UserLayout>
