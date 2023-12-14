4971f1f155ad63515192f999ba1258dcc7bdfb3c
Kirby-b <kirby.brodie66@gmail.com>
2023-12-14 09:43:22 -0500

    Create Technology-&-Privacy.blade.php

---
+++ LearnX/resources/views/lessons/History/Civics/Technology-&-Privacy.blade.php
@@ -0,0 +1,64 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+                <!-- Links Section -->
+                <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                    </p>
+                    <p class="text-xl font-sans m-4">
+                        <a href="{{ route('page.holidays') }}" class="hover:text-purple-300">Holidays Around the World</a><br>
+                        <br>
+                        <a href="{{ route('page.contenents.oceans') }}" class="hover:text-purple-300">Contenents/Oceans</a><br>
+                        <br>
+                        <a href="{{ route('page.states') }}" class="hover:text-purple-300">States in the U.S.</a><br>
+                        <br>
+                        <a href="{{ route('page.branches.of.gov') }}" class="hover:text-purple-300">Branches of the U.S. Government</a><br>
+                        <br>
+                        <a href="{{ route('page.ancient.history') }}" class="hover:text-purple-300">Ancient History</a><br>
+                        <br>
+                        <a href="{{ route('page.american.history') }}" class="hover:text-purple-300">American History</a><br>
+                        <br>
+                        <a href="{{ route('page.world.history') }}" class="hover:text-purple-300">World History</a><br>
+                        <br>
+                        <a href="{{ route('page.economy') }}" class="hover:text-purple-300">Economics</a><br>
+                        <br>
+                        <a href="{{ route('page.civics') }}" class="hover:text-purple-300">Civics</a><br>
+                        <br>
+                        <a href="{{ route('page.politics') }}" class="hover:text-purple-300">Politics</a><br>
+                        <br>
+                    </p>
+                </div>
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                        Civics:<br>
+                        Technology & Privacy:<br>
+                        <br>
+                        Data Protection Laws - Analyzing and discussing the impact and effectiveness of data protection laws, such as the General Data Protection Regulation (GDPR) and the California Consumer Privacy Act (CCPA).<br>
+                        <br>
+                        Surveillance Technologies - Examining the implications of surveillance technologies, including government surveillance, facial recognition, and the use of surveillance cameras in public spaces.<br>
+                        <br>
+                        Internet Privacy - Exploring issues related to internet privacy, including online tracking, data collection by tech companies, and privacy concerns associated with social media platforms.<br>
+                        <br>
+                        Biometric Data and Privacy - Assessing the collection and use of biometric data, such as fingerprints and facial recognition, and the privacy implications of biometric technology.<br>
+                        <br>
+                        Smart Devices and IoT Privacy - Investigating the privacy challenges associated with smart devices and the Internet of Things (IoT), including connected home devices, wearables, and smart city infrastructure.<br>
+                        <br>
+                        Cybersecurity and Personal Information - Addressing cybersecurity issues and the protection of personal information from data breaches, hacks, and cyber threats.<br>
+                        <br>
+                        Ethical Use of AI - Discussing ethical considerations in the development and deployment of artificial intelligence (AI) technologies, including concerns related to bias, transparency, and accountability.<br>
+                        <br>
+                        Government Access to Personal Data - Analyzing the balance between national security interests and individual privacy rights, especially in the context of government access to personal data for law enforcement purposes.<br>
+                        <br>
+                        Employee Privacy in the Workplace - Examining the privacy rights of employees in the workplace, particularly with the increasing use of monitoring technologies by employers.<br>
+                        <br>
+                        Privacy in Health Tech - Discussing privacy concerns related to health technologies, electronic health records, and the sharing of sensitive health information.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
