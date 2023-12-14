1d8a2f11c7797007abf477e491fb6642d66d5913
Kirby-b <kirby.brodie66@gmail.com>
2023-12-14 09:42:52 -0500

    Create Civic-Infrastructure-&-Urban-Planning.blade.php

---
+++ LearnX/resources/views/lessons/History/Civics/Civic-Infrastructure-&-Urban-Planning.blade.php
@@ -0,0 +1,64 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+                <!-- Links Section -->
+                <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                    </p>
+                    <p class="text-xl font-sans m-4">
+                        <a href="{{ route('page.holidays') }}" class="hover:text-purple-300">Holidays Around the World</a><br>
+                        <br>
+                        <a href="{{ route('page.contenents.oceans') }}" class="hover:text-purple-300">Contenents/Oceans</a><br>
+                        <br>
+                        <a href="{{ route('page.states') }}" class="hover:text-purple-300">States in the U.S.</a><br>
+                        <br>
+                        <a href="{{ route('page.branches.of.gov') }}" class="hover:text-purple-300">Branches of the U.S. Government</a><br>
+                        <br>
+                        <a href="{{ route('page.ancient.history') }}" class="hover:text-purple-300">Ancient History</a><br>
+                        <br>
+                        <a href="{{ route('page.american.history') }}" class="hover:text-purple-300">American History</a><br>
+                        <br>
+                        <a href="{{ route('page.world.history') }}" class="hover:text-purple-300">World History</a><br>
+                        <br>
+                        <a href="{{ route('page.economy') }}" class="hover:text-purple-300">Economics</a><br>
+                        <br>
+                        <a href="{{ route('page.civics') }}" class="hover:text-purple-300">Civics</a><br>
+                        <br>
+                        <a href="{{ route('page.politics') }}" class="hover:text-purple-300">Politics</a><br>
+                        <br>
+                    </p>
+                </div>
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                        Civics:<br>
+                        Civic Infrastructure & Urban Planning:<br>
+                        <br>
+                        Smart Cities and Technology Integration - Exploring the integration of technology to enhance urban efficiency, sustainability, and the quality of life for residents.<br>
+                        <br>
+                        Transit-Oriented Development - Focusing on urban planning strategies that prioritize the development of public transportation systems and create walkable, mixed-use communities around transit hubs.<br>
+                        <br>
+                        Sustainable Urban Design - Examining strategies for designing and constructing environmentally friendly and resource-efficient urban spaces.<br>
+                        <br>
+                        Inclusive and Equitable Planning - Addressing issues related to social equity in urban planning.<br>
+                        <br>
+                        Resilient Cities - Developing strategies to enhance urban resilience, considering the impact of climate change, natural disasters, and other shocks and stresses on city infrastructure.<br>
+                        <br>
+                        Mixed-Use Development - Exploring the benefits and challenges of mixed-use developments that combine residential, commercial, and recreational spaces to create vibrant and inclusive urban environments.<br>
+                        <br>
+                        Public Space Design - Analyzing the design and management of public spaces, including parks, plazas, and waterfronts, to enhance community well-being and social interaction.<br>
+                        <br>
+                        Transportation Planning and Mobility - Addressing challenges related to traffic congestion, sustainable transportation options, and the integration of new mobility solutions, such as bike-sharing and electric scooters.<br>
+                        <br>
+                        Urban Regeneration and Revitalization - Examining strategies for revitalizing and regenerating urban areas, including brownfield redevelopment, adaptive reuse, and efforts to preserve cultural heritage.<br>
+                        <br>
+                        Community-Driven Planning - Focusing on community engagement and participatory planning processes that involve residents in decision-making, ensuring that urban developments meet the needs and aspirations of the local population.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
