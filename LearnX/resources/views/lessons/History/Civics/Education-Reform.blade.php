82564130c473238d7a739cfb8d718a3dd718a7c6
Kirby-b <kirby.brodie66@gmail.com>
2023-12-14 09:43:11 -0500

    Update Education-Reform.blade.php

--- LearnX/resources/views/lessons/History/Civics/Education-Reform.blade.php
+++ LearnX/resources/views/lessons/History/Civics/Education-Reform.blade.php
@@ -37,36 +37,25 @@
                         Civics:<br>
                         Education Reform:<br>
                         <br>
-                        Digital Learning and Technology Integration:
-
-Exploring the integration of technology in education, including online learning platforms, digital resources, and the impact of technology on teaching methods.
-Equitable Access to Education:
-
-Addressing disparities in educational access, resources, and opportunities, and advocating for policies that promote equity in education.
-Curriculum Development and Modernization:
-
-Evaluating and updating curricula to reflect contemporary knowledge, skills, and global perspectives, and considering the integration of STEAM (Science, Technology, Engineering, Arts, and Mathematics) subjects.
-Teacher Training and Professional Development:
-
-Enhancing teacher training programs, providing ongoing professional development, and supporting teachers in adapting to new educational approaches.
-Standardized Testing and Assessment:
-
-Examining the role of standardized testing in education, its impact on students and teachers, and exploring alternative methods of assessment.
-Inclusive Education:
-
-Promoting inclusive practices that cater to diverse learning needs, including those of students with disabilities, and fostering a supportive learning environment for all.
-Early Childhood Education:
-
-Emphasizing the importance of early childhood education, exploring effective preschool programs, and considering the impact of early learning experiences on later academic success.
-Education Funding and Resource Allocation:
-
-Analyzing funding models for education, addressing disparities in resource allocation, and advocating for sufficient and equitable funding for schools.
-School Safety and Mental Health Support:
-
-Addressing issues related to school safety, including bullying and violence, and providing mental health resources and support for students.
-Parental and Community Involvement:
-
-Promoting collaboration between schools, parents, and communities to create a supportive educational ecosystem and enhance student success.
+                        Digital Learning and Technology Integration - Exploring the integration of technology in education and the impact of technology on teaching methods.<br>
+                        <br>
+                        Equitable Access to Education - Addressing disparities in educational access, resources, and opportunities, and advocating for policies that promote equity in education.<br>
+                        <br>
+                        Curriculum Development and Modernization - Evaluating and updating curricula to reflect contemporary knowledge, skills, and global perspectives.<br>
+                        <br>
+                        Teacher Training and Professional Development - Enhancing teacher training programs, providing ongoing professional development, and supporting teachers in adapting to new educational approaches.<br>
+                        <br>
+                        Standardized Testing and Assessment - Examining the role of standardized testing in education, its impact on students and teachers, and exploring alternative methods of assessment.<br>
+                        <br>
+                        Inclusive Education - Promoting inclusive practices that cater to diverse learning needs, including those of students with disabilities, and fostering a supportive learning environment for all.<br>
+                        <br>
+                        Early Childhood Education - Emphasizing the importance of early childhood education, exploring effective preschool programs, and considering the impact of early learning experiences on later academic success.<br>
+                        <br>
+                        Education Funding and Resource Allocation - Analyzing funding models for education, addressing disparities in resource allocation, and advocating for sufficient and equitable funding for schools.<br>
+                        <br>
+                        School Safety and Mental Health Support - Addressing issues related to school safety, including bullying and violence, and providing mental health resources and support for students.<br>
+                        <br>
+                        Parental and Community Involvement - Promoting collaboration between schools, parents, and communities to create a supportive educational ecosystem and enhance student success.
                     </p>
                 </div>
             </div>
