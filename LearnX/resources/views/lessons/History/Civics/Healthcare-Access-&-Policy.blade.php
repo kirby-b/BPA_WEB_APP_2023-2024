a7ab514c67151c530b1e9d3837933a8ad1c2bf87
Kirby-b <kirby.brodie66@gmail.com>
2023-12-14 09:43:15 -0500

    Create Healthcare-Access-&-Policy.blade.php

---
+++ LearnX/resources/views/lessons/History/Civics/Healthcare-Access-&-Policy.blade.php
@@ -0,0 +1,64 @@
+<x-partials.UserLayout>
+    <div class="bg-gray-100 py-10">
+        <div class="container mx-auto px-4">
+            <div class="flex flex-col md:flex-row justify-center items-center md:space-x-6">
+                <!-- Links Section -->
+                <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                    </p>
+                    <p class="text-xl font-sans m-4">
+                        <a href="{{ route('page.holidays') }}" class="hover:text-purple-300">Holidays Around the World</a><br>
+                        <br>
+                        <a href="{{ route('page.contenents.oceans') }}" class="hover:text-purple-300">Contenents/Oceans</a><br>
+                        <br>
+                        <a href="{{ route('page.states') }}" class="hover:text-purple-300">States in the U.S.</a><br>
+                        <br>
+                        <a href="{{ route('page.branches.of.gov') }}" class="hover:text-purple-300">Branches of the U.S. Government</a><br>
+                        <br>
+                        <a href="{{ route('page.ancient.history') }}" class="hover:text-purple-300">Ancient History</a><br>
+                        <br>
+                        <a href="{{ route('page.american.history') }}" class="hover:text-purple-300">American History</a><br>
+                        <br>
+                        <a href="{{ route('page.world.history') }}" class="hover:text-purple-300">World History</a><br>
+                        <br>
+                        <a href="{{ route('page.economy') }}" class="hover:text-purple-300">Economics</a><br>
+                        <br>
+                        <a href="{{ route('page.civics') }}" class="hover:text-purple-300">Civics</a><br>
+                        <br>
+                        <a href="{{ route('page.politics') }}" class="hover:text-purple-300">Politics</a><br>
+                        <br>
+                    </p>
+                </div>
+                <!-- Text Section -->
+                <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
+                    <p class="text-xl font-sans m-4">
+                        History:<br>
+                        Civics:<br>
+                        Healthcare Access and Policy:<br>
+                        <br>
+                        Universal Healthcare - Examining models and approaches to achieve universal healthcare coverage, ensuring that all individuals have access to essential healthcare services.<br>
+                        <br>
+                        Healthcare Disparities - Addressing disparities in healthcare access and outcomes based on factors such as race, ethnicity, socioeconomic status, and geographic location.<br>
+                        <br>
+                        Health Insurance Reform - Analyzing and proposing reforms to health insurance systems to enhance coverage, affordability, and the overall efficiency of healthcare delivery.<br>
+                        <br>
+                        Mental Health Parity - Advocating for policies that ensure mental health services receive equal consideration in terms of coverage, accessibility, and reimbursement compared to physical health services.<br>
+                        <br>
+                        Telemedicine and Digital Health - Exploring the role of telemedicine and digital health technologies in expanding healthcare access, especially in remote or underserved areas.<br>
+                        <br>
+                        Primary Care and Preventive Services - Focusing on the importance of primary care, preventive services, and community health initiatives to reduce the burden of preventable diseases.<br>
+                        <br>
+                        Healthcare Workforce Planning - Addressing challenges related to the recruitment, training, and distribution of healthcare professionals to meet the evolving needs of communities.<br>
+                        <br>
+                        Pharmaceutical Pricing and Access - Examining the pricing and accessibility of pharmaceuticals, exploring policies to control drug costs and ensure affordable access to essential medications.<br>
+                        <br>
+                        Public Health Emergency Preparedness - Developing and evaluating policies to enhance healthcare system readiness and responsiveness to public health emergencies, such as pandemics or natural disasters.<br>
+                        <br>
+                        Elderly and Long-Term Care - Addressing the healthcare needs of the elderly population, including policies related to long-term care, caregiving, and support services for aging individuals.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-partials.UserLayout>
