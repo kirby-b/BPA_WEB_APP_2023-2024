6565ceb3f7771b57224e477f78e1a3e24e02a035
Kirby-b <kirby.brodie66@gmail.com>
2023-12-14 09:42:02 -0500

    Update Politics.blade.php

--- LearnX/resources/views/lessons/History/Politics.blade.php
+++ LearnX/resources/views/lessons/History/Politics.blade.php
@@ -33,7 +33,28 @@
                 <!-- Text Section -->
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg">
                     <p class="text-xl font-sans m-4">
-                        
+                        History:<br>
+                        Politics:<br>
+                        <br>
+                        Global Governance and International Relations - Examining the relationships between countries, international organizations, and the role of diplomacy in addressing global challenges.<br>
+                        <br>
+                        Political Ideologies - Analyzing various political ideologies, such as liberalism, conservatism, socialism, and nationalism, and their impact on political thought and policy.<br>
+                        <br>
+                        Elections and Electoral Systems:Studying the electoral processes, including voting systems, campaign strategies, and the impact of elections on political landscapes.<br>
+                        <br>
+                        Political Economy:Exploring the intersection of politics and economics, including policies related to taxation, trade, and economic development.<br>
+                        <br>
+                        Policy Analysis and Public Administration:Investigating the development, implementation, and evaluation of public policies and the role of public administration in governance.<br>
+                        <br>
+                        Human Rights and Social Justice:Addressing issues related to human rights, social justice, and equality, and exploring policies aimed at promoting inclusivity and addressing discrimination.<br>
+                        <br>
+                        Political Communication:Examining the role of media, propaganda, and political communication strategies in shaping public opinion and influencing political outcomes.<br>
+                        <br>
+                        Political Parties and Party Systems:Analyzing the structure and dynamics of political parties, coalition building, and the role of political parties in representative democracies.<br>
+                        <br>
+                        Political Institutions:Studying the structures and functions of political institutions, including legislatures, executives, and judiciaries, and their role in governance.<br>
+                        <br>
+                        Political Philosophy and Political Theory:Exploring foundational ideas and theories in political philosophy, including works by political thinkers such as Plato, Aristotle, John Locke, and others.
                     </p>
                 </div>
             </div>
