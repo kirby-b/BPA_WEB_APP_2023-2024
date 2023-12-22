f423718f84a04de4f368451a67133ab99191750f
kirby-b <kirby.brodie66@gmail.com>
2023-12-22 12:43:33 -0500

    Update Physics.blade.php

--- LearnX/resources/views/lessons/Science/Physics.blade.php
+++ LearnX/resources/views/lessons/Science/Physics.blade.php
@@ -12,7 +12,7 @@
                         <br>
                         <a href="{{ route('page.space') }}" class="hover:text-purple-300">Space</a><br>
                         <br>
-                        <a href="{{ route('page.life') }}" class="hover:text-purple-300">Life</a><br>
+                        <a href="{{ route('page.biology') }}" class="hover:text-purple-300">Biology</a><br>
                         <br>
                         <a href="{{ route('page.animals') }}" class="hover:text-purple-300">Animals</a><br>
                         <br>
@@ -35,6 +35,21 @@
                     <p class="text-xl font-sans m-4">
                         <span class="font-black">Science:</span><br>
                         <span class="font-bold">Physics:</span><br>
+                        Classical Mechanics: Classical mechanics, formulated by Isaac Newton, describes the motion of objects and the forces acting upon them. It includes concepts such as Newton's laws of motion, gravity, and the study of motion in various contexts.<br>
+
+                        Thermodynamics: Thermodynamics explores the relationship between heat, energy, and work. It includes the laws of thermodynamics, the behavior of gases, phase transitions, and the principles governing heat engines and refrigeration.<br>
+
+                        Electromagnetism: Electromagnetism studies the interaction between electrically charged particles and magnetic fields. It encompasses Maxwell's equations, which describe how electric and magnetic fields are generated and how they propagate.<br>
+
+                        Optics: Optics focuses on the behavior of light and its interaction with matter. It covers topics such as reflection, refraction, diffraction, and the behavior of lenses and mirrors.<br>
+
+                        Quantum Mechanics: Quantum mechanics deals with the behavior of matter and energy on atomic and subatomic scales. It introduces concepts like wave-particle duality, uncertainty principle, and quantum states, revolutionizing our understanding of fundamental particles and their interactions.<br>
+
+                        Nuclear Physics: Nuclear physics explores the properties and interactions of atomic nuclei, including nuclear reactions, radioactive decay, and the structure of atomic nuclei.<br>
+
+                        Particle Physics: Particle physics studies the fundamental particles and forces that make up the universe. It investigates particles such as quarks, leptons, bosons, and the interactions governed by fundamental forces like gravity, electromagnetism, weak, and strong nuclear forces.<br>
+
+                        Astrophysics and Cosmology: Astrophysics examines the properties and phenomena of celestial objects, galaxies, stars, and the universe as a whole. Cosmology focuses on the origin, evolution, and structure of the universe itself.<br>
                     </p>
                 </div>
             </div>
