fbad1aae133a72f9d685aa001693f94398bd31e1
kirby-b <kirby.brodie66@gmail.com>
2023-12-23 12:05:19 -0500

    Update Numbers.blade.php
    I dont know if this is what is wanted but we will see

--- LearnX/resources/views/lessons/Math/Numbers.blade.php
+++ LearnX/resources/views/lessons/Math/Numbers.blade.php
@@ -34,7 +34,39 @@
                     <p class="text-xl font-sans m-4">
                         <span class="font-black">Math:</span><br>
                         <span class="font-bold">Numbers:</span><br>
-                        
+                        <h2>Types of Numbers:</h2>
+                        <ul>
+                            <li>Natural Numbers (N): These are the counting numbers (1, 2, 3, 4, ...).</li>
+                            <li>Whole Numbers (W): Include zero along with natural numbers (0, 1, 2, 3, ...).</li>
+                            <li>Integers (Z): Include positive, negative numbers, and zero (-3, -2, -1, 0, 1, 2, 3, ...).</li>
+                            <li>Rational Numbers (Q): Numbers that can be expressed as a fraction of integers (such as 1/2, -3/4, 7, etc.).</li>
+                            <li>Irrational Numbers: Numbers that cannot be expressed as a fraction and have non-repeating, non-terminating decimal representations (like π or √2).</li>
+                            <li>Real Numbers (R): Include all rational and irrational numbers, filling the number line completely.</li>
+                            <li>Complex Numbers (C): Numbers of the form a + bi, where 'a' and 'b' are real numbers and 'i' is the imaginary unit (√(-1)).</li>
+                        </ul>
+
+                        <h2>Number Operations:</h2>
+                        <ul>
+                            <li>Addition (+): Combining numbers.</li>
+                            <li>Subtraction (-): Taking away one number from another.</li>
+                            <li>Multiplication (×): Repeated addition or grouping.</li>
+                            <li>Division (÷): Sharing or splitting into equal parts.</li>
+                            <li>Exponents and Roots: Representing repeated multiplication or inverse operations of raising to a power.</li>
+                        </ul>
+                        <h2>Number Properties:</h2>
+                        <ul>
+                            <li>Commutative Property: Changing the order of numbers doesn't change the result in addition and multiplication (a + b = b + a, ab = ba).</li>
+                            <li>Associative Property: Changing the grouping of numbers doesn't change the result in addition and multiplication ((a + b) + c = a + (b + c), (ab)c = a(bc)).</li>
+                            <li>Distributive Property: Multiplication distributes over addition/subtraction (a * (b + c) = ab + ac).</li>
+                            <li>Identity Element: Numbers that, when combined with another using a specific operation, don't change the value (0 is the additive identity, 1 is the multiplicative identity).</li>
+                        </ul>
+                        <h2>Number Systems:</h2>
+                        <ul>
+                            <li>Binary: Base-2 numbering system using 0s and 1s.</li>
+                            <li>Octal: Base-8 numbering system.</li>
+                            <li>Decimal: Base-10 numbering system (our everyday system).</li>
+                            <li>Hexadecimal: Base-16 numbering system using digits 0-9 and letters A-F.</li>
+                        </ul>
                     </p>
                 </div>
             </div>
