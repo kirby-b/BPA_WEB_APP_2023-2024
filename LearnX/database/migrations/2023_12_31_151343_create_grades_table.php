f9f9d06184089e439090bc4bcd91908e23fc5121
kirby-b <kirby.brodie66@gmail.com>
2023-12-31 10:15:17 -0500

    Create 2023_12_31_151343_create_grades_table.php

---
+++ LearnX/database/migrations/2023_12_31_151343_create_grades_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('grades', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('overall_grade');
+            $table->string('math');
+            $table->string('science');
+            $table->string('english');
+            $table->string('history');
+            $table->string('extras');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('grades');
+    }
+};
