eaffc42dfd0db092f6b4a8e968761f96e4a89a53
Kirby-b <kirby.brodie66@gmail.com>
2024-01-03 10:21:55 -0500

    Update Flashcards-Science.blade.php

--- LearnX/resources/views/lessons/Science/InteractiveLearning/Flashcards-Science.blade.php
+++ LearnX/resources/views/lessons/Science/InteractiveLearning/Flashcards-Science.blade.php
@@ -25,52 +25,13 @@
                             <b>Flash Cards</b>
                         </div>
                         {{-- Generate a quizlet essentially --}}
-                        <div 
-                            class="max-w-4xl mx-auto relative"
-                            x-data="{ activeSlide: 1, slides: [1, 2, 3, 4, 5] }"
-                        >
-                            <div id="1" x-data="{ flipped: false }" class="py-32 relative w-96 h-96 mx-auto mt-8 mb-8 cursor-pointer text-center font-bold tracking-light text-lg">
-                                                    <div x-show.transition.scale.5.duration.400ms="!flipped" class="absolute text-center py-16 bg-gradient-to-br from-purple-500 via-violet-300 to-transparent overflow-hidden inset-0 rounded-lg shadow-lg" @click="flipped = true;" >
-                                                        Front
-                                                    </div>
-                                                    <div x-show.transition.scale.5.duration.400ms="flipped" style="display: none;" class="absolute text-center py-16 bg-gradient-to-br from-white via-teal-200 to-green-500 bg-blue-300 overflow-hidden inset-0 rounded-lg shadow-lg" @click="flipped = false;" >
-                                                        Back
-                                                    </div>
-                                                </div>
-                            <!-- Prev/Next Arrows -->
-                            <div class="absolute inset-0 flex">
-                            <div class="flex items-center justify-start w-1/2">
-                                <button 
-                                class="bg-teal-100 text-teal-500 hover:text-orange-500 font-bold hover:shadow-lg rounded-full w-12 h-12 -ml-6"
-                                x-on:click="activeSlide = activeSlide === 1 ? slides.length : activeSlide - 1">
-                                &#8592;
-                                </button>
+                        <div id="1" x-data="{ flipped: false }" class="py-32 relative w-96 h-96 mx-auto mt-8 mb-8 cursor-pointer text-center font-bold tracking-light text-lg">
+                            <div x-show.transition.scale.5.duration.400ms="!flipped" class="absolute text-center py-16 bg-gradient-to-br from-purple-500 via-violet-300 to-transparent overflow-hidden inset-0 rounded-lg shadow-lg" @click="flipped = true;" >
+                                Front
                             </div>
-                            <div class="flex items-center justify-end w-1/2">
-                                <button 
-                                class="bg-teal-100 text-teal-500 hover:text-orange-500 font-bold hover:shadow rounded-full w-12 h-12 -mr-6"
-                                x-on:click="activeSlide = activeSlide === slides.length ? 1 : activeSlide + 1">
-                                &#8594;
-                                </button>
-                            </div>        
+                            <div x-show.transition.scale.5.duration.400ms="flipped" style="display: none;" class="absolute text-center py-16 bg-gradient-to-br from-white via-teal-200 to-green-500 bg-blue-300 overflow-hidden inset-0 rounded-lg shadow-lg" @click="flipped = false;" >
+                                Back
                             </div>
-
-                            <!-- Buttons -->
-                            <div class="absolute w-full flex items-center justify-center px-4">
-                            <template x-for="slide in slides" :key="slide">
-                                <button
-                                class="flex-1 w-4 h-2 mt-4 mx-2 mb-0 rounded-full overflow-hidden transition-colors duration-200 ease-out hover:bg-teal-600 hover:shadow-lg"
-                                :class="{ 
-                                    'bg-orange-600': activeSlide === slide,
-                                    'bg-teal-300': activeSlide !== slide 
-                                }" 
-                                x-on:click="activeSlide = slide"
-                                ></button>
-                            </template>
-                            </div>
-                        </div>
-                        
-                        </div>
                         </div>
                     </p>
                 </div>
