78bc86bf7b6ffe73cb8f2122034aa9459525107c
Kirby-b <kirby.brodie66@gmail.com>
2024-01-03 10:22:01 -0500

    Update DeleteAccount.blade.php

--- LearnX/resources/views/users/DeleteAccount.blade.php
+++ LearnX/resources/views/users/DeleteAccount.blade.php
@@ -7,7 +7,7 @@
                 <div class="md:w-1/2 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg flex justify-center items-center">
                     <div class="text-xl font-sans m-4">
                         <p class="text-xl mb-1">Account Termination</p>
-                        <p class="text-base mb-1">To confirm please put in your name, username, and password</p>
+                        <p class="text-base mb-1">To confirm please put in your username and password</p>
                         <!--Goes to the delete route and then to the UserController-->
                         <form method="POST" action="/delete">
                             @csrf <!--Makes it so people cant use scripts against your site-->
