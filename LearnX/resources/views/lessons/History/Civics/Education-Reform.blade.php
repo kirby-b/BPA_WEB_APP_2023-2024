95bba991ef54afc1f5b773a825e0dcb7467185cc
Kirby-b <kirby.brodie66@gmail.com>
2024-01-05 10:17:26 -0500

    Civics link bar

--- LearnX/resources/views/lessons/History/Civics/Education-Reform.blade.php
+++ LearnX/resources/views/lessons/History/Civics/Education-Reform.blade.php
@@ -5,29 +5,29 @@
                 <!-- Links Section -->
                 <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
                     <p class="text-xl font-sans m-4">
-                        History:<br>
+                        <a href="{{ route('page.history') }}" class="hover:text-purple-300">History:</a><br>
+                        <a href="{{ route('page.civics') }}" class="hover:text-purple-300">Civics:</a><br>
                     </p>
                     <p class="text-xl font-sans m-4">
-                        <a href="{{ route('page.holidays') }}" class="hover:text-purple-300">Holidays Around the World</a><br>
+                        <a href="{{ route('page.voting.rights') }}" class="hover:text-purple-300">Voting Rights and Election Integrity</a><br>
                         <br>
-                        <a href="{{ route('page.contenents.oceans') }}" class="hover:text-purple-300">Contenents/Oceans</a><br>
+                        <a href="{{ route('page.civics.engagement') }}" class="hover:text-purple-300">Civic Engagement and Participation</a><br>
                         <br>
-                        <a href="{{ route('page.states') }}" class="hover:text-purple-300">States in the U.S.</a><br>
+                        <a href="{{ route('page.governmentTrans') }}" class="hover:text-purple-300">Government Transparency</a><br>
                         <br>
-                        <a href="{{ route('page.branches.of.gov') }}" class="hover:text-purple-300">Branches of the U.S. Government</a><br>
+                        <a href="{{ route('page.social.justice') }}" class="hover:text-purple-300">Social Justice and Equity</a><br>
                         <br>
-                        <a href="{{ route('page.ancient.history') }}" class="hover:text-purple-300">Ancient History</a><br>
+                        <a href="{{ route('page.climate') }}" class="hover:text-purple-300">Climate Change and Environmental Sustainability</a><br>
                         <br>
-                        <a href="{{ route('page.american.history') }}" class="hover:text-purple-300">American History</a><br>
+                        <a href="{{ route('page.education.reform') }}" class="hover:text-purple-300">Education Reform</a><br>
                         <br>
-                        <a href="{{ route('page.world.history') }}" class="hover:text-purple-300">World History</a><br>
+                        <a href="{{ route('page.health.access') }}" class="hover:text-purple-300">Healthcare Access and Policy</a><br>
                         <br>
-                        <a href="{{ route('page.economy') }}" class="hover:text-purple-300">Economics</a><br>
+                        <a href="{{ route('page.tech.privacy') }}" class="hover:text-purple-300">Technology and Privacy</a><br>
                         <br>
-                        <a href="{{ route('page.civics') }}" class="hover:text-purple-300">Civics</a><br>
+                        <a href="{{ route('page.emergency.management') }}" class="hover:text-purple-300">Crisis Response and Emergency Management</a><br>
                         <br>
-                        <a href="{{ route('page.politics') }}" class="hover:text-purple-300">Politics</a><br>
-                        <br>
+                        <a href="{{ route('page.civic.urban') }}" class="hover:text-purple-300">Civic Infrastructure and Urban Planning</a>
                     </p>
                 </div>
                 <!-- Text Section -->
