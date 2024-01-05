805c66c9a90c06175251347c4c15214c6f503eb4
Kirby-b <kirby.brodie66@gmail.com>
2024-01-05 10:16:57 -0500

    American history link bar

--- LearnX/resources/views/lessons/History/American/17th-Century.blade.php
+++ LearnX/resources/views/lessons/History/American/17th-Century.blade.php
@@ -5,8 +5,8 @@
                 <!-- Links Section -->
                 <div class="md:w-1/4 p-4 bg-white border-2 border-gray-300 rounded-lg shadow-lg md:items-start md:self-start">
                     <p class="text-xl font-sans m-4">
-                        History:<br>
-                        American History:<br>
+                        <a href="{{ route('page.history') }}" class="hover:text-purple-300">History:</a><br>
+                        <a href="{{ route('page.american.history') }}" class="hover:text-purple-300">American History:</a><br>
                     </p>
                     <p class="text-xl font-sans m-4">
                         <br>
