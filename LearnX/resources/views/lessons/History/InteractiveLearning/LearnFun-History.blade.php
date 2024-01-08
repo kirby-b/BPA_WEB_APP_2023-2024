60b20d8b89bd7272f59ccf4fe0743134c3d1d658
kirby-b <kirby.brodie66@gmail.com>
2024-01-08 16:11:18 -0500

    Update LearnFun-History.blade.php
    -Should relatively work
    -Test for quiz
    -Things will change

--- LearnX/resources/views/lessons/History/InteractiveLearning/LearnFun-History.blade.php
+++ LearnX/resources/views/lessons/History/InteractiveLearning/LearnFun-History.blade.php
@@ -35,199 +35,90 @@
                             <input type="radio" id="ans4" name="question1" value="ans4">
                             <label for="ans4">D. ans4</label>
                         </form> --}}
-                        {{-- CSS for quiz. Convert to tailwind
-                            
-                            *
 
-                            .game-quiz-container{
-                                width: 40rem;
-                                height: 30rem;
-                                background-color: lightgray;
-                                display: flex;
-                                flex-direction: column;
-                                align-items: center;
-                                justify-content: space-around;
-                                border-radius: 30px;
-                            }
-
-                            .game-details-container{
-                                width: 80%;
-                                height: 4rem;
-                                display: flex;
-                                justify-content: space-around;
-                                align-items: center;
-                            }
-
-                            .game-details-container h1{
-                                font-size: 1.2rem;
-                            }
-
-                            .game-question-container{
-                                width: 80%;
-                                height: 8rem;
-                                display: flex;
-                                align-items: center;
-                                justify-content: center;
-                                border: 2px solid darkgray;
-                                border-radius: 25px;
-                            }
-
-                            .game-question-container h1{
-                                font-size: 1.1rem;
-                                text-align: center;
-                                padding: 3px;
-                            }
-
-                            .game-options-container{
-                                width: 80%;
-                                height: 12rem;
-                                display: flex;
-                                flex-wrap: wrap;
-                                align-items: center;
-                                justify-content: space-around;
-                            }
-
-                            .game-options-container span{
-                                width: 45%;
-                                height: 3rem;
-                                border: 2px solid darkgray;
-                                border-radius: 20px;
-                                overflow: hidden;
-                            }
-                            span label{
-                                width: 100%;
-                                height: 100%;
-                                display: flex;
-                                align-items: center;
-                                justify-content: center;
-                                cursor: pointer;
-                                transition: transform 0.3s;
-                                font-weight: 600;
-                                color: rgb(22, 22, 22);
-                            }
-
-                            span label:hover{
-                                -ms-transform: scale(1.12);
-                                -webkit-transform: scale(1.12);
-                                transform: scale(1.12);
-                                color: white;
-                            }
-
-                            input[type="radio"] {
-                                position: relative;
-                                display: none;
-                            }
-
-
-                            input[type=radio]:checked ~ .option {
-                                background: paleturquoise;
-                            }
-
-                            .next-button-container{
-                                width: 50%;
-                                height: 3rem;
-                                display: flex;
-                                justify-content: center;
-                            }
-                            .next-button-container button{
-                                width: 8rem;
-                                height: 2rem;
-                                border-radius: 10px;
-                                background: none;
-                                color: rgb(25, 25, 25);
-                                font-weight: 600;
-                                border: 2px solid gray;
-                                cursor: pointer;
-                                outline: none;
-                            }
-                            .next-button-container button:hover{
-                                background-color: rgb(143, 93, 93);
-                            }
-
-                            --}}
-                            <main onload="NextQuestion(0)" class="flex flex-column justify-center align-center bg-black"><!--On load was part of body innitially this may need to be there instead-->
-                                <!-- creating a modal for when quiz ends -->
-                                <div class="fadeIn font-serif z-1 hidden fixed top-0 left-0 overflow-auto bg-black flex-column justify-center align-center" id="score-modal">
-                                  
-                                    <div class="bg-slate-900 flex flex-column align-center justify-around border-2xl">
-                                      
-                                        <h1>Congratulations, Quiz Completed.</h1>
-                                      
-                                        <div class="flex flex-column align-center justify-around">
-                                            <p class="text-white align-center">Attempts : 10</p>
-                                            <p class="text-white align-center">Wrong Answers : <span id="wrong-answers"></span></p>
-                                            <p class="text-white align-center">Right Answers : <span id="right-answers"></span></p>
-                                            <p class="text-white align-center">Grade : <span id="grade-percentage"></span>%</p>
-                                            <p class="text-white align-center"><span id="remarks"></span></p>
-                                        </div>
-                                      
-                                        <div class="hover:bg-slate-400 flex justify-center align-center">
-                                            <button onclick="closeScoreModal()" class="bg-none outline-0 border-solid border-sm bg-orange-100 text-white text-lg border-2xl cursor-pointer">Continue</button>
-                                        </div>
-                                      
+                        <main onload="NextQuestion(0)" class="flex flex-column justify-center align-center bg-black"><!--On load was part of body innitially this may need to be there instead-->
+                            <!-- creating a modal for when quiz ends -->
+                            <div class="fadeIn font-serif z-1 hidden fixed top-0 left-0 overflow-auto bg-black flex-column justify-center align-center" id="score-modal">
+                                
+                                <div class="bg-slate-900 flex flex-column align-center justify-around rounded-2xl">
+                                    
+                                    <h1>Congratulations, Quiz Completed.</h1>
+                                    
+                                    <div class="flex flex-column align-center justify-around">
+                                        <p class="text-white align-center">Attempts : 10</p>
+                                        <p class="text-white align-center">Wrong Answers : <span id="wrong-answers"></span></p>
+                                        <p class="text-white align-center">Right Answers : <span id="right-answers"></span></p>
+                                        <p class="text-white align-center">Grade : <span id="grade-percentage"></span>%</p>
+                                        <p class="text-white align-center"><span id="remarks"></span></p>
                                     </div>
+                                    
+                                    <div class="hover:bg-slate-400 flex justify-center align-center">
+                                        <button onclick="closeScoreModal()" class="bg-none outline-0 border-solid border-2 bg-orange-100 text-white text-lg cursor-pointer rounded-2xl">Continue</button>
+                                    </div>
+                                    
                                 </div>
-                        
-                                <div class="game-quiz-container">
-                                  
-                                    <div class="game-details-container">
-                                        <h1>Score : <span id="player-score"></span> / 10</h1>
-                                        <h1> Question : <span id="question-number"></span> / 10</h1>
-                                    </div>
-                        
-                                    <div class="game-question-container">
-                                        <h1 id="display-question"></h1>
-                                    </div>
-                        
-                                    <div class="game-options-container">
-                                      
-                                       <div class="fadeIn font-serif z-1 hidden fixed top-0 left-0 overflow-auto bg-black flex-column justify-center align-center" id="option-modal">
-                                         
-                                            <div class="bg-slate-900 flex flex-column align-center justify-around border-2xl">
-                                                 <h1 class="text-gray-500 align-center">Please Pick An Option</h1>
-                                              
-                                                 <div class="hover:bg-slate-400 flex justify-center align-center">
-                                                    <button onclick="closeOptionModal()">Continue</button>
-                                                </div>
-                                              
+                            </div>
+                    
+                            <div class="bg-slate-400 flex flex-column align-center justify-around rounded-3xl">
+                                
+                                <div class="flex justify-around align-center">
+                                    <h1 class="text-lg">Score : <span id="player-score"></span> / 10</h1>
+                                    <h1 class="text-lg"> Question : <span id="question-number"></span> / 10</h1>
+                                </div>
+                    
+                                <div class="flex align-center justify-center border-2 border-solid rounded-3xl">
+                                    <h1 id="display-question" class="text-lg text-center p-3"></h1>
+                                </div>
+                    
+                                <div class="flex flex-wrap align-center justify-around">
+                                    
+                                    <div class="fadeIn font-serif z-1 hidden fixed top-0 left-0 overflow-auto bg-black flex-column justify-center align-center" id="option-modal">
+                                        
+                                        <div class="bg-slate-900 flex flex-column align-center justify-around rounded-2xl">
+                                                <h1 class="text-gray-500 align-center">Please Pick An Option</h1>
+                                            
+                                                <div class="hover:bg-slate-400 flex justify-center align-center">
+                                                <button onclick="closeOptionModal()">Continue</button>
                                             </div>
-                                         
-                                       </div>
-                                      
-                                        <span>
-                                            <input type="radio" id="option-one" name="option" class="radio" value="optionA" />
-                                            <label for="option-one" class="option" id="option-one-label"></label>
-                                        </span>
-                                      
-                        
-                                        <span>
-                                            <input type="radio" id="option-two" name="option" class="radio" value="optionB" />
-                                            <label for="option-two" class="option" id="option-two-label"></label>
-                                        </span>
-                                      
-                        
-                                        <span>
-                                            <input type="radio" id="option-three" name="option" class="radio" value="optionC" />
-                                            <label for="option-three" class="option" id="option-three-label"></label>
-                                        </span>
-                                      
-                        
-                                        <span>
-                                            <input type="radio" id="option-four" name="option" class="radio" value="optionD" />
-                                            <label for="option-four" class="option" id="option-four-label"></label>
-                                        </span>
-                        
-                        
+                                            
+                                        </div>
+                                        
                                     </div>
-                        
-                                    <div class="next-button-container">
-                                        <button onclick="handleNextQuestion()">Next Question</button>
-                                    </div>
-                        
+                                    
+                                    <span class="border-2 border-solid rounded-xl overflow-hidden">
+                                        <input type="radio" id="option-one" name="option" class="radio checked:bg-cyan-300 relative hidden" value="optionA" />
+                                        <label for="option-one" class="option hover:scale flex align-center justify-center cursor-pointer transition-transform text-semibold bg-slate-800" id="option-one-label"></label>
+                                    </span>
+                                    
+                    
+                                    <span class="border-2 border-solid rounded-xl overflow-hidden">
+                                        <input type="radio" id="option-two" name="option" class="radio checked:bg-cyan-300 relative hidden" value="optionB" />
+                                        <label for="option-two" class="option hover:scale flex align-center justify-center cursor-pointer transition-transform text-semibold bg-slate-800" id="option-two-label"></label>
+                                    </span>
+                                    
+                    
+                                    <span class="border-2 border-solid rounded-xl overflow-hidden">
+                                        <input type="radio" id="option-three" name="option" class="radio checked:bg-cyan-300 relative hidden" value="optionC" />
+                                        <label for="option-three" class="option hover:scale flex align-center justify-center cursor-pointer transition-transform text-semibold bg-slate-800" id="option-three-label"></label>
+                                    </span>
+                                    
+                    
+                                    <span class="border-2 border-solid rounded-xl overflow-hidden">
+                                        <input type="radio" id="option-four" name="option" class="radio checked:bg-cyan-300 relative hidden" value="optionD" />
+                                        <label for="option-four" class="option hover:scale flex align-center justify-center cursor-pointer transition-transform text-semibold bg-slate-800" id="option-four-label"></label>
+                                    </span>
+                    
+                    
                                 </div>
-                            </main>
-                            <script src="index.js"></script>
-                        {{-- Put questions here in mutiple choice format --}}
+                    
+                                <div class="flex justify-center">
+                                    <button onclick="handleNextQuestion()" class="hover:bg-rose-800 cursor-pointer border-2 bg-none outline-0 border-solid border-base rounded-lg">Next Question</button>
+                                </div>
+                    
+                            </div>
+                        </main>
+                        <script src="index.js"></script>
+                    {{-- Put questions here in mutiple choice format --}}
                     </p>
                 </div>
             </div>
