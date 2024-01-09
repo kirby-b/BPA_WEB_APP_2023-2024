5995c7a0f072f7625b62b53d4c73a9f00c63a0ef
kirby-b <kirby.brodie66@gmail.com>
2024-01-09 18:19:37 -0500

    Update LearnFun-Math.blade.php

--- LearnX/resources/views/lessons/Math/InteractiveLearning/LearnFun-Math.blade.php
+++ LearnX/resources/views/lessons/Math/InteractiveLearning/LearnFun-Math.blade.php
@@ -37,28 +37,26 @@
                         </form> --}}
                         <div>
                             <div class="min-h-screen flex items-center justify-center bg-black bg-no-repeat bg-cover bg-center">
-                                <!-- Modal for quiz completion -->
-                                <div class="modal-container hidden fixed inset-0 z-10 bg-black bg-opacity-40 flex items-center justify-center" id="score-modal">
+                                <div class="modal-container hidden fixed inset-0 z-10 bg-black bg-opacity-40 flex items-center justify-center" id="score-modal"><!--Displays Score when quiz is over-->
                                     <div class="modal-content-container bg-gray-800 rounded-lg p-6">
                                         <h1 class="text-gray-300 text-lg mb-4">Congratulations, Quiz Completed.</h1>
                                         <div class="grade-details text-white text-center">
-                                            <p>Attempts : 10</p>
-                                            <p>Wrong Answers : <span id="wrong-answers"></span></p>
-                                            <p>Right Answers : <span id="right-answers"></span></p>
-                                            <p>Grade : <span id="grade-percentage"></span>%</p>
-                                            <p><span id="remarks"></span></p>
+                                            <p>Attempts : 2</p>
+                                            <p>Wrong Answers : <span id="wrong-answers"></span></p><!--Displays number or wrong answers-->
+                                            <p>Right Answers : <span id="right-answers"></span></p><!--Displays number or right answers-->
+                                            <p>Grade : <span id="grade-percentage"></span>%</p><!--Displays gade percent-->
+                                            <p><span id="remarks"></span></p> <!--Displays remark based on grade-->
                                         </div>
                                         <div class="modal-button-container flex justify-center mt-4">
-                                            <button onclick="closeScoreModal()" class="bg-gray-800 border border-white text-white px-4 py-1 rounded-lg hover:bg-gray-700">Continue</button>
+                                            <button onclick="closeScoreModal()" class="bg-gray-800 border border-white text-white px-4 py-1 rounded-lg hover:bg-gray-700">Continue</button><!--Closes the score screen and starts the test again-->
                                         </div>
                                     </div>
                                 </div>
                         
                                 <div class="game-quiz-container w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 bg-gray-300 flex flex-col items-center justify-around rounded-lg">
                                     <div class="game-details-container w-4/5 flex justify-around items-center">
-                                        <h1>Score : <span id="player-score"></span> / 10</h1>
-                                        <h1>Question : <span id="question-number"></span> / 10</h1>
-                                    </div>
+                                        <h1>Score : <span id="player-score"></span> / 10</h1><!--Displays number or right answers-->
+                                        <h1>Question : <span id="question-number"></span> / 10</h1><!--Displays number or right answersquestion number-->
                                     <div class="game-question-container w-4/5 h-32 flex items-center justify-center border-2 border-gray-700 rounded-lg">
                                         <h1 id="display-question" class="text-base text-center"></h1>
                                     </div>
@@ -87,9 +85,10 @@
                                             <input type="radio" id="option-four" name="option" class="radio hidden" value="optionD" />
                                             <label for="option-four" class="option block w-full h-12 border-2 border-gray-700 rounded-full overflow-hidden cursor-pointer transition-transform duration-300 ease-in-out font-semibold text-gray-900 text-center leading-12 hover:scale-110 text-sm" id="option-four-label"></label>
                                         </span>
+                                        <!--The spans above display the answer buttons-->
                                     </div>
                                     <div class="next-button-container w-1/2 flex justify-center">
-                                        <button onclick="handleNextQuestion()" class="w-28 h-8 rounded-lg border border-gray-700 bg-transparent text-gray-900 font-semibold cursor-pointer hover:bg-red-500 hover:text-white transition-all duration-300 focus:outline-none text-sm">Next Question</button>
+                                        <button onclick="handleNextQuestion()" class="w-28 h-8 rounded-lg border border-gray-700 bg-transparent text-gray-900 font-semibold cursor-pointer hover:bg-red-500 hover:text-white transition-all duration-300 focus:outline-none text-sm">Next Question</button><!--Displays the next question-->
                                     </div>
                                 </div>
                             </div>
@@ -104,236 +103,100 @@
   
   
   
-    <script type="text/javascript">
+    <script type="text/javascript">//Script for all javascript
       function codeAddress() {
           alert('ok');
       }
   
-      const questions = [
+      const questions = [//Contains all of the quiz questions. Formatted as Question, four answers, and a correct answer
           {
-              question: "How many days makes a week ?",
-              optionA: "10 days",
-              optionB: "14 days",
-              optionC: "5 days",
-              optionD: "7 days",
+              question: "What is the term used to represent the mathematical expression consisting of numbers, variables, and mathematical operations like addition, subtraction, multiplication, and division?",
+              optionA: "Equation",
+              optionB: "Function",
+              optionC: "Polynomial",
+              optionD: "Expression",
               correctOption: "optionD"
           },
-  
+
           {
-              question: "How many players are allowed on a soccer pitch ?",
-              optionA: "10 players",
-              optionB: "11 players",
-              optionC: "9 players",
-              optionD: "12 players",
+              question: "In calculus, what is the derivative of a constant?",
+              optionA: "0",
+              optionB: "1",
+              optionC: "The constant itself",
+              optionD: "Undefined",
+              correctOption: "optionA"
+          },
+         
+          {
+              question: "What is the result of multiplying any number by zero?",
+              optionA: "The number itself",
+              optionB: "Zero",
+              optionC: "One",
+              optionD: "Undefined",
               correctOption: "optionB"
           },
-  
+
           {
-              question: "Who was the first President of USA ?",
-              optionA: "Donald Trump",
-              optionB: "Barack Obama",
-              optionC: "Abraham Lincoln",
-              optionD: "George Washington",
-              correctOption: "optionD"
-          },
-  
-          {
-              question: "30 days has ______ ?",
-              optionA: "January",
-              optionB: "December",
-              optionC: "June",
-              optionD: "August",
+              question: "What is the decimal equivalent of 3/4?",
+              optionA: "0.25",
+              optionB: "0.5",
+              optionC: "0.75",
+              optionD: "0.333",
               correctOption: "optionC"
           },
-  
+          
           {
-              question: "How manay hours can be found in a day ?",
-              optionA: "30 hours",
-              optionB: "38 hours",
-              optionC: "48 hours",
-              optionD: "24 hours",
-              correctOption: "optionD"
-          },
-  
-          {
-              question: "Which is the longest river in the world ?",
-              optionA: "River Nile",
-              optionB: "Long River",
-              optionC: "River Niger",
-              optionD: "Lake Chad",
-              correctOption: "optionA"
-          },
-  
-          {
-              question: "_____ is the hottest Continent on Earth ?",
-              optionA: "Oceania",
-              optionB: "Antarctica",
-              optionC: "Africa",
-              optionD: "North America",
-              correctOption: "optionC"
-          },
-  
-          {
-              question: "Which country is the largest in the world ?",
-              optionA: "Russia",
-              optionB: "Canada",
-              optionC: "Africa",
-              optionD: "Egypt",
-              correctOption: "optionA"
-          },
-  
-          {
-              question: "Which of these numbers is an odd number ?",
-              optionA: "Ten",
-              optionB: "Twelve",
-              optionC: "Eight",
-              optionD: "Eleven",
-              correctOption: "optionD"
-          },
-  
-          {
-              question: `"You Can't see me" is a popular saying by`,
-              optionA: "Eminem",
-              optionB: "Bill Gates",
-              optionC: "Chris Brown",
-              optionD: "John Cena",
-              correctOption: "optionD"
-          },
-  
-          {
-              question: "Where is the world tallest building located ?",
-              optionA: "Africa",
-              optionB: "California",
-              optionC: "Dubai",
-              optionD: "Italy",
-              correctOption: "optionC"
-          },
-  
-          {
-              question: "The longest river in the United Kingdom is ?",
-              optionA: "River Severn",
-              optionB: "River Mersey",
-              optionC: "River Trent",
-              optionD: "River Tweed",
-              correctOption: "optionA"
-          },
-  
-  
-          {
-              question: "How many permanent teeth does a dog have ?",
-              optionA: "38",
-              optionB: "42",
-              optionC: "40",
-              optionD: "36",
+              question: "What is the name for a polygon with six sides?",
+              optionA: "Octagon",
+              optionB: "Hexagon",
+              optionC: "Heptagon",
+              optionD: "Decagon",
               correctOption: "optionB"
           },
-  
+          
           {
-              question: "Which national team won the football World cup in 2018 ?",
-              optionA: "England",
-              optionB: "Brazil",
-              optionC: "Germany",
-              optionD: "France",
+              question: "What type of graph is used to show the relationship between two variables, typically displaying a straight-line correlation?",
+              optionA: "Bar graph",
+              optionB: "Pie chart",
+              optionC: "Scatter plot",
+              optionD: "Line graph",
               correctOption: "optionD"
           },
-  
+          
           {
-              question: "Which US state was Donald Trump Born ?",
-              optionA: "New York",
-              optionB: "California",
-              optionC: "New Jersey",
-              optionD: "Los Angeles",
-              correctOption: "optionA"
+              question: "Which type of number can be expressed as a ratio of two integers, where the denominator is not zero?",
+              optionA: "Irrational number",
+              optionB: "Rational number",
+              optionC: "Prime number",
+              optionD: "Imaginary number",
+              correctOption: "optionB"
           },
-  
+          
           {
-              question: "How man states does Nigeria have ?",
+              question: "What is the next number in the sequence: 2, 5, 10, 17, ...?",
               optionA: "24",
-              optionB: "30",
-              optionC: "36",
-              optionD: "37",
-              correctOption: "optionC"
-          },
-  
-          {
-              question: "____ is the capital of Nigeria ?",
-              optionA: "Abuja",
-              optionB: "Lagos",
-              optionC: "Calabar",
-              optionD: "Kano",
-              correctOption: "optionA"
-          },
-  
-          {
-              question: "Los Angeles is also known as ?",
-              optionA: "Angels City",
-              optionB: "Shining city",
-              optionC: "City of Angels",
-              optionD: "Lost Angels",
-              correctOption: "optionC"
-          },
-  
-          {
-              question: "What is the capital of Germany ?",
-              optionA: "Georgia",
-              optionB: "Missouri",
-              optionC: "Oklahoma",
-              optionD: "Berlin",
+              optionB: "26",
+              optionC: "28",
+              optionD: "29",
               correctOption: "optionD"
           },
-  
+          
           {
-              question: "How many sides does an hexagon have ?",
-              optionA: "Six",
-              optionB: "Sevene",
-              optionC: "Four",
-              optionD: "Five",
+              question: "What is the probability of rolling an even number on a six-sided die?",
+              optionA: "1/3",
+              optionB: "1/2",
+              optionC: "2/3",
+              optionD: "1/6",
+              correctOption: "optionB"
+          },
+          
+          {
+              question: "What value is affected the most by extreme values or outliers in a data set?",
+              optionA: "Mean",
+              optionB: "Median",
+              optionC: "Mode",
+              optionD: "Range",
               correctOption: "optionA"
-          },
-  
-          {
-              question: "How many planets are currently in the solar system ?",
-              optionA: "Eleven",
-              optionB: "Seven",
-              optionC: "Nine",
-              optionD: "Eight",
-              correctOption: "optionD"
-          },
-  
-          {
-              question: "Which Planet is the hottest ?",
-              optionA: "Jupitar",
-              optionB: "Mercury",
-              optionC: "Earth",
-              optionD: "Venus",
-              correctOption: "optionB"
-          },
-  
-          {
-              question: "where is the smallest bone in human body located?",
-              optionA: "Toes",
-              optionB: "Ears",
-              optionC: "Fingers",
-              optionD: "Nose",
-              correctOption: "optionB"
-          },
-  
-          {
-              question: "How many hearts does an Octopus have ?",
-              optionA: "One",
-              optionB: "Two",
-              optionC: "Three",
-              optionD: "Four",
-              correctOption: "optionC"
-          },
-  
-          {
-              question: "How many teeth does an adult human have ?",
-              optionA: "28",
-              optionB: "30",
-              optionC: "32",
-              optionD: "36",
-              correctOption: "optionC"
           }
   
       ]
