d1045fa45c5b4591a67767a4f80dda01698dd807
kirby-b <kirby.brodie66@gmail.com>
2024-01-09 18:19:41 -0500

    Update LearnFun-Science.blade.php

--- LearnX/resources/views/lessons/Science/InteractiveLearning/LearnFun-Science.blade.php
+++ LearnX/resources/views/lessons/Science/InteractiveLearning/LearnFun-Science.blade.php
@@ -85,28 +85,26 @@
                         </form> --}}
                         <div>
                             <div class="min-h-screen flex items-center justify-center bg-black bg-no-repeat bg-cover bg-center">
-                                <!-- Modal for quiz completion -->
-                                <div class="modal-container hidden fixed inset-0 z-10 bg-black bg-opacity-40 flex items-center justify-center" id="score-modal">
+                                <div class="modal-container hidden fixed inset-0 z-10 bg-black bg-opacity-40 flex items-center justify-center" id="score-modal"><!--Displays Score when quiz is over-->
                                     <div class="modal-content-container bg-gray-800 rounded-lg p-6">
                                         <h1 class="text-gray-300 text-lg mb-4">Congratulations, Quiz Completed.</h1>
                                         <div class="grade-details text-white text-center">
-                                            <p>Attempts : 10</p>
-                                            <p>Wrong Answers : <span id="wrong-answers"></span></p>
-                                            <p>Right Answers : <span id="right-answers"></span></p>
-                                            <p>Grade : <span id="grade-percentage"></span>%</p>
-                                            <p><span id="remarks"></span></p>
+                                            <p>Attempts : 2</p>
+                                            <p>Wrong Answers : <span id="wrong-answers"></span></p><!--Displays number or wrong answers-->
+                                            <p>Right Answers : <span id="right-answers"></span></p><!--Displays number or right answers-->
+                                            <p>Grade : <span id="grade-percentage"></span>%</p><!--Displays gade percent-->
+                                            <p><span id="remarks"></span></p> <!--Displays remark based on grade-->
                                         </div>
                                         <div class="modal-button-container flex justify-center mt-4">
-                                            <button onclick="closeScoreModal()" class="bg-gray-800 border border-white text-white px-4 py-1 rounded-lg hover:bg-gray-700">Continue</button>
+                                            <button onclick="closeScoreModal()" class="bg-gray-800 border border-white text-white px-4 py-1 rounded-lg hover:bg-gray-700">Continue</button><!--Closes the score screen and starts the test again-->
                                         </div>
                                     </div>
                                 </div>
                         
                                 <div class="game-quiz-container w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-2/5 bg-gray-300 flex flex-col items-center justify-around rounded-lg">
                                     <div class="game-details-container w-4/5 flex justify-around items-center">
-                                        <h1>Score : <span id="player-score"></span> / 10</h1>
-                                        <h1>Question : <span id="question-number"></span> / 10</h1>
-                                    </div>
+                                        <h1>Score : <span id="player-score"></span> / 10</h1><!--Displays number or right answers-->
+                                        <h1>Question : <span id="question-number"></span> / 10</h1><!--Displays number or right answersquestion number-->
                                     <div class="game-question-container w-4/5 h-32 flex items-center justify-center border-2 border-gray-700 rounded-lg">
                                         <h1 id="display-question" class="text-base text-center"></h1>
                                     </div>
@@ -135,9 +133,10 @@
                                             <input type="radio" id="option-four" name="option" class="radio hidden" value="optionD" />
                                             <label for="option-four" class="option block w-full h-12 border-2 border-gray-700 rounded-full overflow-hidden cursor-pointer transition-transform duration-300 ease-in-out font-semibold text-gray-900 text-center leading-12 hover:scale-110 text-sm" id="option-four-label"></label>
                                         </span>
+                                        <!--The spans above display the answer buttons-->
                                     </div>
                                     <div class="next-button-container w-1/2 flex justify-center">
-                                        <button onclick="handleNextQuestion()" class="w-28 h-8 rounded-lg border border-gray-700 bg-transparent text-gray-900 font-semibold cursor-pointer hover:bg-red-500 hover:text-white transition-all duration-300 focus:outline-none text-sm">Next Question</button>
+                                        <button onclick="handleNextQuestion()" class="w-28 h-8 rounded-lg border border-gray-700 bg-transparent text-gray-900 font-semibold cursor-pointer hover:bg-red-500 hover:text-white transition-all duration-300 focus:outline-none text-sm">Next Question</button><!--Displays the next question-->
                                     </div>
                                 </div>
                             </div>
@@ -152,12 +151,12 @@
   
   
   
-    <script type="text/javascript">
+    <script type="text/javascript">//Script for all javascript
       function codeAddress() {
           alert('ok');
       }
   
-      const questions = [
+      const questions = [//Contains all of the quiz questions. Formatted as Question, four answers, and a correct answer
           { 
               question: "Which biotechnological process involves the production of genetically identical copies of a particular gene or DNA sequence?",
               optionA: "Cloning",
